e0e0209aca46c8eea6481544deeb2d17d0f58a1c
webeweb <support@webeweb.fr>
2018-01-06 15:36:33 +0100

    Add PHPUnit

---
+++ Tests/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+if (file_exists($file = __DIR__ . "/autoload.php")) {
+	require_once $file;
+} elseif (file_exists($file = __DIR__ . "/autoload.php.dist")) {
+	require_once $file;
+}
