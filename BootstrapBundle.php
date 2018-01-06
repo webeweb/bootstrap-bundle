e54741e7355f39af9626197f6ed289d78cba6070
webeweb <support@webeweb.fr>
2018-01-06 15:37:59 +0100

    Add main bundle class

---
+++ BootstrapBundle.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bootstrap bundle.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle
+ * @final
+ */
+final class BootstrapBundle extends Bundle {
+
+}
