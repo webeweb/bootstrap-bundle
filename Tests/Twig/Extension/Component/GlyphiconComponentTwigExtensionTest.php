446ae7471074d00290f9d00b984c5a2fbc4d64e6
webeweb <support@webeweb.fr>
2018-01-09 18:31:59 +0100

    Add glyhicon component twig extension

---
+++ Tests/Twig/Extension/Component/GlyphiconComponentTwigExtensionTest.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Component;
+
+use PHPUnit_Framework_TestCase;
+use Twig_Node;
+use Twig_SimpleFunction;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\GlyphiconComponentTwigExtension;
+
+/**
+ * Glyphicon component Twig extension test.
+ *
+ *  @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Component
+ * @final
+ */
+final class GlyphiconComponentTwigExtensionTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Tests the getFunctions() method.
+	 *
+	 * @return void
+	 */
+	public function testGetFunctions() {
+
+		$obj = new GlyphiconComponentTwigExtension();
+
+		$res = $obj->getFunctions();
+
+		$this->assertCount(1, $res);
+
+		$this->assertInstanceOf(Twig_SimpleFunction::class, $res[0]);
+		$this->assertEquals("bootstrapGlyphicon", $res[0]->getName());
+		$this->assertEquals([$obj, "bootstrapGlyphiconFunction"], $res[0]->getCallable());
+		$this->assertEquals(["html"], $res[0]->getSafe(new Twig_Node()));
+	}
+
+	/**
+	 * Tests the bootstrapGlyphiconFunction() method.
+	 *
+	 * @return void
+	 * @depends testGetFunctions
+	 */
+	public function testBootstrapGlyphiconFunction() {
+
+		$obj = new GlyphiconComponentTwigExtension();
+
+		$arg0	 = [];
+		$res0	 = '<span class="glyphicon glyphicon-home" aria-hidden="true"></span>';
+		$this->assertEquals($res0, $obj->bootstrapGlyphiconFunction($arg0));
+
+		$arg1	 = ["name" => "asterisk"];
+		$res1	 = '<span class="glyphicon glyphicon-asterisk" aria-hidden="true"></span>';
+		$this->assertEquals($res1, $obj->bootstrapGlyphiconFunction($arg1));
+	}
+
+}
---
+++ Twig/Extension/Component/GlyphiconComponentTwigExtension.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Twig\Extension\Component;
+
+use Twig_SimpleFunction;
+use WBW\Library\Core\Utility\ArrayUtility;
+
+/**
+ * Glyphicon component Twig extension.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Twig\Extension\Component
+ * @final
+ */
+final class GlyphiconComponentTwigExtension extends AbstractComponentTwigExtension {
+
+	/**
+	 * Service name.
+	 *
+	 * @var string
+	 */
+	const SERVICE_NAME = "webeweb.bundle.bootstrapbundle.twig.extension.component.glyphicon";
+
+	/**
+	 * Displays a Bootstrap glyphicon.
+	 *
+	 * @param array $args The arguments.
+	 * @return string Returns the Bootstrap glyphicon.
+	 */
+	public function bootstrapGlyphiconFunction(array $args = []) {
+		return $this->bootstrapGlyphicon(ArrayUtility::get($args, "name", "home"));
+	}
+
+	/**
+	 * Get the Twig functions.
+	 *
+	 * @return array Returns the Twig functions.
+	 */
+	public function getFunctions() {
+		return [
+			new Twig_SimpleFunction("bootstrapGlyphicon", [$this, "bootstrapGlyphiconFunction"], ["is_safe" => ["html"]]),
+		];
+	}
+
+}
