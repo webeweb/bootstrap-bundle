56cf9a29c215b6af4469360ab5fabd6b9b2930cb
webeweb <support@webeweb.fr>
2018-01-09 18:30:55 +0100

    Add abstract bootstrap tiwg extension

---
+++ Twig/Extension/AbstractBootstrapTwigExtension.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Twig\Extension;
+
+use Twig_Extension;
+
+/**
+ * Abstract Bootstrap Twig extension.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Twig\Extension
+ * @abstract
+ */
+abstract class AbstractBootstrapTwigExtension extends Twig_Extension {
+
+	/**
+	 * Default content.
+	 *
+	 * @var string
+	 */
+	const DEFAULT_CONTENT = "&nbsp;";
+
+}
