a377b304c4708b9d9e31efd9833c39723f87dd5a
webeweb <support@webeweb.fr>
2018-02-20 11:08:43 +0100

    Add typography Twig extensions

--- Tests/DependencyInjection/BootstrapExtensionTest.php
+++ Tests/DependencyInjection/BootstrapExtensionTest.php
@@ -24,6 +24,15 @@ use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\ButtonComponentTwigExten
 use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\GlyphiconComponentTwigExtension;
 use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\LabelComponentTwigExtension;
 use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\ProgressBarComponentTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Typography\DeletedTypographyTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Typography\HeadingTypographyTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Typography\InsertedTypographyTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Typography\ItalicTypographyTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Typography\MarkedTypographyTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Typography\SmallTypographyTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Typography\StrikeThroughTypographyTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Typography\StrongTypographyTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Typography\UnderlinedTypographyTwigExtension;
 
 /**
  * Bootstrap extension test.
@@ -54,12 +63,23 @@ final class BootstrapExtensionTest extends PHPUnit_Framework_TestCase {
         $obj = new BootstrapExtension();
 
         $obj->load([], $container);
+        // Component
         $this->assertInstanceOf(AlertComponentTwigExtension::class, $container->get(AlertComponentTwigExtension::SERVICE_NAME));
         $this->assertInstanceOf(BadgeComponentTwigExtension::class, $container->get(BadgeComponentTwigExtension::SERVICE_NAME));
         $this->assertInstanceOf(ButtonComponentTwigExtension::class, $container->get(ButtonComponentTwigExtension::SERVICE_NAME));
         $this->assertInstanceOf(GlyphiconComponentTwigExtension::class, $container->get(GlyphiconComponentTwigExtension::SERVICE_NAME));
         $this->assertInstanceOf(LabelComponentTwigExtension::class, $container->get(LabelComponentTwigExtension::SERVICE_NAME));
         $this->assertInstanceOf(ProgressBarComponentTwigExtension::class, $container->get(ProgressBarComponentTwigExtension::SERVICE_NAME));
+        // Typography
+        $this->assertInstanceOf(DeletedTypographyTwigExtension::class, $container->get(DeletedTypographyTwigExtension::SERVICE_NAME));
+        $this->assertInstanceOf(HeadingTypographyTwigExtension::class, $container->get(HeadingTypographyTwigExtension::SERVICE_NAME));
+        $this->assertInstanceOf(InsertedTypographyTwigExtension::class, $container->get(InsertedTypographyTwigExtension::SERVICE_NAME));
+        $this->assertInstanceOf(ItalicTypographyTwigExtension::class, $container->get(ItalicTypographyTwigExtension::SERVICE_NAME));
+        $this->assertInstanceOf(MarkedTypographyTwigExtension::class, $container->get(MarkedTypographyTwigExtension::SERVICE_NAME));
+        $this->assertInstanceOf(SmallTypographyTwigExtension::class, $container->get(SmallTypographyTwigExtension::SERVICE_NAME));
+        $this->assertInstanceOf(StrikeThroughTypographyTwigExtension::class, $container->get(StrikeThroughTypographyTwigExtension::SERVICE_NAME));
+        $this->assertInstanceOf(StrongTypographyTwigExtension::class, $container->get(StrongTypographyTwigExtension::SERVICE_NAME));
+        $this->assertInstanceOf(UnderlinedTypographyTwigExtension::class, $container->get(UnderlinedTypographyTwigExtension::SERVICE_NAME));
     }
 
 }
