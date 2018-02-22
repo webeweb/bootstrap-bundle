3f3d99ea5324b56908f3681600f631973d0fffb4
webeweb <support@webeweb.fr>
2018-02-22 10:32:32 +0100

    Add User input code Twig extension

---
+++ Tests/Twig/Extension/Code/UserInputCodeTwigExtensionTest.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Code;
+
+use PHPUnit_Framework_TestCase;
+use Twig_Node;
+use Twig_SimpleFunction;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Code\UserInputCodeTwigExtension;
+
+/**
+ * User input code Twig extension test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Code
+ * @final
+ */
+final class UserInputCodeTwigExtensionTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Tests the getFunctions() method.
+     *
+     * @return void
+     */
+    public function testGetFunctions() {
+
+        $obj = new UserInputCodeTwigExtension();
+
+        $res = $obj->getFunctions();
+
+        $this->assertCount(1, $res);
+
+        $this->assertInstanceOf(Twig_SimpleFunction::class, $res[0]);
+        $this->assertEquals("bootstrapUserInput", $res[0]->getName());
+        $this->assertEquals([$obj, "bootstrapUserInputFunction"], $res[0]->getCallable());
+        $this->assertEquals(["html"], $res[0]->getSafe(new Twig_Node()));
+    }
+
+    /**
+     * Tests the bootstrapUserInputFunction() method.
+     *
+     * @return void
+     * @depends testGetFunctions
+     */
+    public function testBootstrapUserInputFunction() {
+
+        $obj = new UserInputCodeTwigExtension();
+
+        $arg0 = [];
+        $res0 = "<kbd></kbd>";
+        $this->assertEquals($res0, $obj->bootstrapUserInputFunction($arg0));
+
+        $arg9 = ["content" => "content"];
+        $res9 = "<kbd>content</kbd>";
+        $this->assertEquals($res9, $obj->bootstrapUserInputFunction($arg9));
+    }
+
+}
