c22112b260d8a2f454074f85ffcfe1d44da7a802
webeweb <support@webeweb.fr>
2018-02-22 10:29:51 +0100

    Add Basic block code Twig extension

---
+++ Twig/Extension/Code/BasicBlockCodeTwigExtension.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Twig\Extension\Code;
+
+use Twig_SimpleFunction;
+use WBW\Library\Core\Utility\ArrayUtility;
+
+/**
+ * Basic block code Twig extension.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Twig\Extension\Code
+ * @final
+ */
+final class BasicBlockCodeTwigExtension extends AbstractCodeTwigExtension {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "webeweb.bundle.bootstrapbundle.twig.extension.code.basicblock";
+
+    /**
+     * Displays a Bootstrap basic block.
+     *
+     * @param array $args The arguments.
+     * @return string Returns the Bootstrap basic block.
+     */
+    public function bootstrapBasicBlockFunction(array $args = []) {
+        return $this->bootstrapBasicBlock(ArrayUtility::get($args, "content"));
+    }
+
+    /**
+     * Get the Twig functions.
+     *
+     * @return array Returns the Twig functions.
+     */
+    public function getFunctions() {
+        return [
+            new Twig_SimpleFunction("bootstrapBasicBlock", [$this, "bootstrapBasicBlockFunction"], ["is_safe" => ["html"]]),
+        ];
+    }
+
+}
