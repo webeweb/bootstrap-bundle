0977a2016058bfef2a87d60fa1fc122d89ce6f6f
webeweb <support@webeweb.fr>
2018-02-22 10:30:25 +0100

    Add Sample output code Twig extension

---
+++ Twig/Extension/Code/SampleOutputCodeTwigExtension.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Twig\Extension\Code;
+
+use Twig_SimpleFunction;
+use WBW\Library\Core\Utility\ArrayUtility;
+
+/**
+ * Sample output code Twig extension.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Twig\Extension\Code
+ * @final
+ */
+final class SampleOutputCodeTwigExtension extends AbstractCodeTwigExtension {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "webeweb.bundle.bootstrapbundle.twig.extension.code.sampleoutput";
+
+    /**
+     * Displays a Bootstrap inline.
+     *
+     * @param array $args The arguments.
+     * @return string Returns the Bootstrap inline.
+     */
+    public function bootstrapSampleOutputFunction(array $args = []) {
+        return $this->bootstrapSampleOutput(ArrayUtility::get($args, "content"));
+    }
+
+    /**
+     * Get the Twig functions.
+     *
+     * @return array Returns the Twig functions.
+     */
+    public function getFunctions() {
+        return [
+            new Twig_SimpleFunction("bootstrapSampleOutput", [$this, "bootstrapSampleOutputFunction"], ["is_safe" => ["html"]]),
+        ];
+    }
+
+}
