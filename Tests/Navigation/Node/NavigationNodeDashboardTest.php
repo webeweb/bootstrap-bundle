8780edaf593cf336075c608cd5870eec936aeeb4
webeweb <support@webeweb.fr>
2018-03-06 20:42:52 +0100

    Add Navigation node "Dashboard"

---
+++ Tests/Navigation/Node/NavigationNodeDashboardTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Navigation\Node;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Bundle\BootstrapBundle\Navigation\Node\NavigationNodeDashboard;
+
+/**
+ * Navigation node "Dashboard" test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Navigation\Node
+ * @final
+ */
+final class NavigationNodeDashboardTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        $obj = new NavigationNodeDashboard("route");
+
+        $this->assertEquals("navigation.node.dashboard", $obj->getId());
+        $this->assertEquals("dashboard", $obj->getIcon());
+        $this->assertEquals("route", $obj->getRoute());
+    }
+
+}
