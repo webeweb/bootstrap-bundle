59029a248a0d39fa0529ec8860e5e239e88b867f
webeweb <support@webeweb.fr>
2018-03-06 20:44:08 +0100

    Add Navigation node edit action

---
+++ Tests/Navigation/Node/Action/NavigationNodeEditActionTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Navigation\Node\Action;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Bundle\BootstrapBundle\Navigation\Node\Action\NavigationNodeEditAction;
+
+/**
+ * Navigation node "Edit" action test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Navigation\Node\Action
+ * @final
+ */
+final class NavigationNodeEditActionTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        $obj = new NavigationNodeEditAction("route");
+
+        $this->assertEquals("navigation.node.action.edit", $obj->getId());
+        $this->assertEquals("pencil", $obj->getIcon());
+        $this->assertEquals("route", $obj->getRoute());
+    }
+
+}
