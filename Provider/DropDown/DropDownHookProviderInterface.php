1185f55499a7c852498e936e9e077322b21ffc77
webeweb <support@webeweb.fr>
2018-03-06 20:31:41 +0100

    Add Drop down hook provider interface

---
+++ Provider/DropDown/DropDownHookProviderInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Provider\DropDown;
+
+/**
+ * Drop down hook provider interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Provider\DropDown
+ */
+interface DropDownHookProviderInterface {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "webeweb.bundle.bootstrapbundle.provider.dropdownhook";
+
+    /**
+     * Get the items.
+     *
+     * @return array Returns the items.
+     */
+    public function getItems();
+
+    /**
+     * Get the template.
+     *
+     * @return string Returns the template.
+     */
+    public function getTemplate();
+}
