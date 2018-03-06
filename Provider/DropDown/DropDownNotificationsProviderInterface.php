0b19726ad0d1dd2d96e491cedb6ad896d02ad03a
webeweb <support@webeweb.fr>
2018-03-06 20:31:50 +0100

    Add Drop down notifications provider interface

---
+++ Provider/DropDown/DropDownNotificationsProviderInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Provider\DropDown;
+
+/**
+ * Drop down "Notifications" provider interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Provider\DropDown
+ */
+interface DropDownNotificationsProviderInterface {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "webeweb.bundle.bootstrapbundle.provider.dropdownnotifications";
+
+    /**
+     * Get the notifications.
+     *
+     * @return array Returns the notifications.
+     */
+    public function getNotifications();
+
+    /**
+     * Get the template.
+     *
+     * @return string Returns the template.
+     */
+    public function getTemplate();
+}
