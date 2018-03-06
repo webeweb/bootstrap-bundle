06cb5f9e3a84f4db2fa3391e4874572801234694
webeweb <support@webeweb.fr>
2018-03-06 20:43:12 +0100

    Add Navigation node "Parameters"

---
+++ Tests/Navigation/Node/NavigationNodeParametersTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Navigation\Node;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Bundle\BootstrapBundle\Navigation\Node\NavigationNodeParameters;
+
+/**
+ * Navigation node "Parameters" test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Navigation\Node
+ * @final
+ */
+final class NavigationNodeParametersTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        $obj = new NavigationNodeParameters("route");
+
+        $this->assertEquals("navigation.node.parameters", $obj->getId());
+        $this->assertEquals("wrench", $obj->getIcon());
+        $this->assertEquals("route", $obj->getRoute());
+    }
+
+}
