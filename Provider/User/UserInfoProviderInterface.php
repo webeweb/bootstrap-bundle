79a9d15a918eaeae0e9762a9776cbda362cecf62
webeweb <support@webeweb.fr>
2018-03-06 20:30:59 +0100

    Add User info provider interface

---
+++ Provider/User/UserInfoProviderInterface.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Provider\User;
+
+/**
+ * User info provider interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Provider\User
+ */
+interface UserInfoProviderInterface {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "webeweb.bundle.bootstrapbundle.provider.userinfo";
+
+    /**
+     * Get the template.
+     *
+     * @return string Returns the template.
+     */
+    public function getTemplate();
+
+    /**
+     * Provides a register link.
+     *
+     * @return boolean Returns true in case of success, false otherwise.
+     */
+    public function provideRegisterLink();
+
+    /**
+     * Provides a resseting link.
+     *
+     * @return boolean Returns true in case of success, false otherwise.
+     */
+    public function provideResettingLink();
+}
