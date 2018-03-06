f9fed4cfb7a4477a38fa4982df7bf8a89284ec8d
webeweb <support@webeweb.fr>
2018-03-06 20:30:12 +0100

    Add Application provider interface

---
+++ Provider/Application/ApplicationProviderInterface.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Provider\Application;
+
+/**
+ * Application provider interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Provider\Application
+ */
+interface ApplicationProviderInterface {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "webeweb.bundle.bootstrapbundle.provider.application";
+
+    /**
+     * Get the description.
+     *
+     * @return string Returns the description.
+     */
+    public function getDescription();
+
+    /**
+     * Get the home.
+     *
+     * @return string Returns the home.
+     */
+    public function getHome();
+
+    /**
+     * Get the name.
+     *
+     * @return string Returns the name.
+     */
+    public function getName();
+
+    /**
+     * Get the route.
+     *
+     * @return string Returns the route.
+     */
+    public function getRoute();
+
+    /**
+     * Get the title.
+     *
+     * @return string Returns the title.
+     */
+    public function getTitle();
+
+    /**
+     * Get the version.
+     *
+     * @return string Returns the version.
+     */
+    public function getVersion();
+
+    /**
+     * Get the year.
+     *
+     * @return string Returns the year.
+     */
+    public function getYear();
+}
