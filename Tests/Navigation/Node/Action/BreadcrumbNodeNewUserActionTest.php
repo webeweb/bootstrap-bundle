f939416c1f117a99e32e372345d9b2a554c92f8d
webeweb <support@webeweb.fr>
2018-03-06 20:46:13 +0100

    Add Breadcrumb node new user action

---
+++ Tests/Navigation/Node/Action/BreadcrumbNodeNewUserActionTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Navigation\Node\Action;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Bundle\BootstrapBundle\Navigation\Node\Action\BreadcrumbNodeNewUserAction;
+
+/**
+ * Breadcrumb node "New user" action test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Navigation\Node\Action
+ * @final
+ */
+final class BreadcrumbNodeNewUserActionTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        $obj = new BreadcrumbNodeNewUserAction("route");
+
+        $this->assertEquals("navigation.node.action.new", $obj->getId());
+        $this->assertEquals("plus", $obj->getIcon());
+        $this->assertEquals("route", $obj->getRoute());
+    }
+
+}
