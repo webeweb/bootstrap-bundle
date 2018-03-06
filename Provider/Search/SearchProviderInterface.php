021702d3c0d768eab96d76f0ae1c9d0ca83dee37
webeweb <support@webeweb.fr>
2018-03-06 20:30:49 +0100

    Add Search provider interface

---
+++ Provider/Search/SearchProviderInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Provider\Search;
+
+/**
+ * Search provider interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Provider\Search
+ */
+interface SearchProviderInterface {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "webeweb.bundle.bootstrapbundle.provider.search";
+
+    /**
+     * Get the template.
+     *
+     * @return string Returns the template.
+     */
+    public function getTemplate();
+}
