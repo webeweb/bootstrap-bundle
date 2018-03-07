11c26e6f678ac42030b38ea5375128fcc1ad468a
webeweb <support@webeweb.fr>
2018-03-07 22:05:47 +0100

    Add Navigation tree

---
+++ Navigation/Tree/NavigationTree.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Navigation\Tree;
+
+use WBW\Bundle\BootstrapBundle\Navigation\Node\AbstractNavigationNode;
+use WBW\Bundle\BootstrapBundle\Navigation\Node\BreadcrumbNode;
+use WBW\Bundle\BootstrapBundle\Navigation\Node\NavigationNode;
+
+/**
+ * Navigation tree.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Navigation\Tree
+ * @final
+ */
+final class NavigationTree extends AbstractNavigationNode {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct("tree");
+    }
+
+    /**
+     * Get the breadcrumbs.
+     *
+     * @param AbstractNavigationNode $node The navigation node.
+     * @return AsbtractNavigationNode[] Returns the navigation nodes.
+     */
+    public function getBreadcrumbs(AbstractNavigationNode $node = null) {
+
+        // Create the breadcrumbs.
+        $breadcrumbs = [];
+
+        // Correct the parameter if necessary.
+        if (null === $node) {
+            $node = $this;
+        }
+
+        // Check the instance.
+        if (true === ($node instanceof NavigationNode || $node instanceof BreadcrumbNode) && true === $node->getActive()) {
+            $breadcrumbs[] = $node;
+        }
+
+        // Handle each node.
+        foreach ($node->getNodes() as $current) {
+            if (false === ($current instanceof AbstractNavigationNode)) {
+                continue;
+            }
+            $breadcrumbs = array_merge($breadcrumbs, $this->getBreadcrumbs($current));
+        }
+
+        // Return the breadcrumbs.
+        return $breadcrumbs;
+    }
+
+}
---
+++ Tests/Navigation/Tree/NavigationTreeTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Navigation\Tree;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Bundle\BootstrapBundle\Navigation\Item\NavigationItem;
+use WBW\Bundle\BootstrapBundle\Navigation\Node\NavigationNode;
+use WBW\Bundle\BootstrapBundle\Navigation\Tree\NavigationTree;
+
+/**
+ * Navigation tree test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Navigation\Node
+ * @final
+ */
+final class NavigationTreeTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Tests the getBreadcrumbs() method.
+     *
+     * @return void
+     */
+    public function testGetBreadcrumbs() {
+
+        $obj = new NavigationTree();
+
+        $obj->addNode(new NavigationNode("main level"));
+        $obj->getLastNode()->setActive(true);
+        $obj->getLastNode()->addNode(new NavigationNode("sub-level1"));
+        $obj->getLastNode()->getLastNode()->setActive(true);
+        $obj->getLastNode()->addNode(new NavigationItem("sub-level2"));
+
+        $res = $obj->getBreadcrumbs();
+        $this->assertCount(2, $res);
+    }
+
+}
