e5f624dca4a79c397add1e5b8067361d0d9482e2
webeweb <support@webeweb.fr>
2018-03-07 22:05:01 +0100

    Add Navigation interface

---
+++ Navigation/NavigationInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Navigation;
+
+/**
+ * Navigation interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Navigation
+ */
+interface NavigationInterface {
+
+    /**
+     * Default href.
+     *
+     * @var string
+     */
+    const DEFAULT_HREF = "javascript: void(0);";
+
+}
