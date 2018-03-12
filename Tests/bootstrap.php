39bc571f7b09685022a3fe9bac1f940ed70ac0dd
webeweb <support@webeweb.fr>
2018-03-12 17:59:57 +0100

    Coding style

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -8,8 +8,8 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-if (file_exists($file = __DIR__ . "/autoload.php")) {
+if (true === file_exists($file = __DIR__ . "/autoload.php")) {
     require_once $file;
-} elseif (file_exists($file = __DIR__ . "/autoload.php.dist")) {
+} elseif (true === file_exists($file = __DIR__ . "/autoload.php.dist")) {
     require_once $file;
 }
