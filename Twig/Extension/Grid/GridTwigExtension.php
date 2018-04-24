6669082311594ce716669f76036408ecc81d379b
webeweb <support@webeweb.fr>
2018-04-24 10:16:35 +0200

    Remove final keyword

--- Twig/Extension/Grid/GridTwigExtension.php
+++ Twig/Extension/Grid/GridTwigExtension.php
@@ -19,9 +19,8 @@ use WBW\Library\Core\Utility\Argument\ArrayUtility;
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\BootstrapBundle\Twig\Extension\Grid
- * @final
  */
-final class GridTwigExtension extends AbstractGridTwigExtension {
+class GridTwigExtension extends AbstractGridTwigExtension {
 
     /**
      * Service name.
