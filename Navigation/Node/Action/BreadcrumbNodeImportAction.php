421aac6b36c8350d392cb3095221db307586bcc6
webeweb <support@webeweb.fr>
2018-04-24 10:13:42 +0200

    Remove final keyword

--- Navigation/Node/Action/BreadcrumbNodeImportAction.php
+++ Navigation/Node/Action/BreadcrumbNodeImportAction.php
@@ -18,9 +18,8 @@ use WBW\Bundle\BootstrapBundle\Navigation\Node\BreadcrumbNode;
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\BootstrapBundle\Navigation\Node\Action
- * @final
  */
-final class BreadcrumbNodeImportAction extends BreadcrumbNode {
+class BreadcrumbNodeImportAction extends BreadcrumbNode {
 
     /**
      * Constructor.
