71266bddda66ffa85a464b7a94f79aa873873ee0
webeweb <support@webeweb.fr>
2018-04-24 10:14:52 +0200

    Remove final keyword

--- Navigation/Node/NavigationNodeDashboard.php
+++ Navigation/Node/NavigationNodeDashboard.php
@@ -18,9 +18,8 @@ use WBW\Bundle\BootstrapBundle\Navigation\Node\NavigationNode;
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\BootstrapBundle\Navigation\Node
- * @final
  */
-final class NavigationNodeDashboard extends NavigationNode {
+class NavigationNodeDashboard extends NavigationNode {
 
     /**
      * Constructor.
