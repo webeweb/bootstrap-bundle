4ae0692bda51c5d4ae0a5edcdec83898c49708a9
webeweb <support@webeweb.fr>
2018-04-24 10:17:23 +0200

    Remove final keyword

--- Twig/Extension/Typography/SmallTypographyTwigExtension.php
+++ Twig/Extension/Typography/SmallTypographyTwigExtension.php
@@ -19,9 +19,8 @@ use WBW\Library\Core\Utility\Argument\ArrayUtility;
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\BootstrapBundle\Twig\Extension\Typography
- * @final
  */
-final class SmallTypographyTwigExtension extends AbstractTypographyTwigExtension {
+class SmallTypographyTwigExtension extends AbstractTypographyTwigExtension {
 
     /**
      * Service name.
