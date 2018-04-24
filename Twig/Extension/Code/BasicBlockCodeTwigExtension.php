badcbcf8a89734eda51e4f7fad86166248ab11e7
webeweb <support@webeweb.fr>
2018-04-24 10:15:38 +0200

    Remove final keyword

--- Twig/Extension/Code/BasicBlockCodeTwigExtension.php
+++ Twig/Extension/Code/BasicBlockCodeTwigExtension.php
@@ -19,9 +19,8 @@ use WBW\Library\Core\Utility\Argument\ArrayUtility;
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\BootstrapBundle\Twig\Extension\Code
- * @final
  */
-final class BasicBlockCodeTwigExtension extends AbstractCodeTwigExtension {
+class BasicBlockCodeTwigExtension extends AbstractCodeTwigExtension {
 
     /**
      * Service name.
