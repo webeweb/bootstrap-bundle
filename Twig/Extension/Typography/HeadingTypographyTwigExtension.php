3f6b0552f1ce900fd64de5bf978f4cb686e3f2e8
webeweb <support@webeweb.fr>
2018-04-24 10:16:55 +0200

    Remove final keyword

--- Twig/Extension/Typography/HeadingTypographyTwigExtension.php
+++ Twig/Extension/Typography/HeadingTypographyTwigExtension.php
@@ -19,9 +19,8 @@ use WBW\Library\Core\Utility\Argument\ArrayUtility;
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\BootstrapBundle\Twig\Extension\Typography
- * @final
  */
-final class HeadingTypographyTwigExtension extends AbstractTypographyTwigExtension {
+class HeadingTypographyTwigExtension extends AbstractTypographyTwigExtension {
 
     /**
      * Service name.
