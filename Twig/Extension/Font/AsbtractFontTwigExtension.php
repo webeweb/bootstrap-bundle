606a68bdb1848da63d762cf998d109a5a17e2634
webeweb <support@webeweb.fr>
2018-04-30 08:17:12 +0200

    Add Abstract font Twig Extension

---
+++ Twig/Extension/Font/AsbtractFontTwigExtension.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Twig\Extension\Font;
+
+use WBW\Bundle\BootstrapBundle\Twig\Extension\AbstractBootstrapTwigExtension;
+
+/**
+ * Asbtract font Twig extension.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Twig\Extension\Font
+ * @abstract
+ */
+abstract class AsbtractFontTwigExtension extends AbstractBootstrapTwigExtension {
+
+    /**
+     * Constructor.
+     */
+    protected function __construct() {
+        parent::__construct();
+    }
+
+}
