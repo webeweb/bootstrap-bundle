73b536c307b8dfaa792857f692aae28394f714a7
webeweb <support@webeweb.fr>
2018-06-02 14:30:27 +0200

    Add abstract Bootstrap event class

---
+++ Event/AbstractBootstrapEvent.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Abstract Bootstrap event.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Event
+ * @abstract
+ */
+abstract class AbstractBootstrapEvent extends Event {
+
+    /**
+     * Event name.
+     *
+     * @var string
+     */
+    private $eventName;
+
+    /**
+     * Constructor.
+     *
+     * @param string $eventName The event name.
+     */
+    protected function __construct($eventName) {
+        $this->eventName = $eventName;
+    }
+
+    /**
+     * Get the event name.
+     *
+     * @return string Returns the event name.
+     */
+    public function getEventName() {
+        return $this->eventName;
+    }
+
+}
