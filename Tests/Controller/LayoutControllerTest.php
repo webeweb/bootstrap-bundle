1e9ab96bcffc669799fb56c021c9afb88b9d919e
webeweb <support@webeweb.fr>
2018-06-04 23:27:42 +0200

    Add a functionnal test for LayoutController

---
+++ Tests/Controller/LayoutControllerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Controller;
+
+use WBW\Bundle\BootstrapBundle\Tests\AbstractBootstrapWebTestCase;
+
+/**
+ * Layout controller test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Controller
+ * @final
+ */
+final class LayoutControllerTest extends AbstractBootstrapWebTestCase {
+
+    /**
+     * Tests the indexAction() method.
+     *
+     * @return void
+     */
+    public function testIndexAction() {
+
+        // Create a client.
+        $client = static::createClient();
+
+        // Make a GET request.
+        $client->request("GET", "/");
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+}
