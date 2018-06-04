0db25bdadabba9fcd7c49f105cea9cb526b3a039
webeweb <support@webeweb.fr>
2018-06-04 20:44:21 +0200

    Add Bootstrap bundle unit tests

---
+++ Tests/BootstrapBundleTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Bundle\BootstrapBundle\BootstrapBundle;
+
+/**
+ * Bootstrap bundle test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests
+ * @final
+ */
+final class BootstrapBundleTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Tests __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        $this->assertEquals("danger", BootstrapBundle::BOOTSTRAP_DANGER);
+        $this->assertEquals("default", BootstrapBundle::BOOTSTRAP_DEFAULT);
+        $this->assertEquals("info", BootstrapBundle::BOOTSTRAP_INFO);
+        $this->assertEquals("primary", BootstrapBundle::BOOTSTRAP_PRIMARY);
+        $this->assertEquals("success", BootstrapBundle::BOOTSTRAP_SUCCESS);
+        $this->assertEquals("warning", BootstrapBundle::BOOTSTRAP_WARNING);
+    }
+
+}
