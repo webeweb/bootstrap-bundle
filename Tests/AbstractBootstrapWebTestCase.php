24bf0cb1c4e06a230a096778e4ce67275b3e82ad
webeweb <support@webeweb.fr>
2018-06-04 23:26:17 +0200

    Create an abstract Bootstrap web test case

---
+++ Tests/AbstractBootstrapWebTestCase.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Abstract Bootstrap web test case.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests
+ * @abstract
+ */
+abstract class AbstractBootstrapWebTestCase extends WebTestCase {
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function setUpBeforeClass() {
+        parent::setUpBeforeClass();
+
+        // Initialize and boot the kernel.
+        static::$kernel = static::createKernel();
+        static::$kernel->boot();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function tearDown() {
+
+        // Shutdown the kernel.
+        static::$kernel->shutdown();
+    }
+
+}
