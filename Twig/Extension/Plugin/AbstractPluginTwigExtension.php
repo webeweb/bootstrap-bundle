24e661bd87e795588a92dd9da8c4aa53548ec04d
webeweb <support@webeweb.fr>
2018-06-11 21:24:16 +0200

    Add Material Design Iconic Font Twig extension

--- Twig/Extension/Plugin/AbstractPluginTwigExtension.php
+++ Twig/Extension/Plugin/AbstractPluginTwigExtension.php
@@ -90,4 +90,47 @@ abstract class AbstractPluginTwigExtension extends AbstractBootstrapTwigExtensio
         return StringUtility::replace(implode("\n", $template), ["%selector%", "%mask%", "%arguments%"], [$selector, $mask, json_encode($options)]);
     }
 
+    /**
+     * Displays a Material Design Iconic Font icon.
+     *
+     * @param string $name The Material Design Iconic Font name.
+     * @param string $size The Material Design Iconic Font size.
+     * @param boolean $fixedWidth Fixed width ?
+     * @param string $border The Material Design Iconic Font border
+     * @param string $pull The Material Design Iconic Font pull.
+     * @param string $spin The Material Design Iconic Font spin.
+     * @param string $rotate The Material Design Iconic Font rotate.
+     * @param string $flip The Material Design Iconic Font flip.
+     * @return string Returns the Material Design Iconic Font icon.
+     */
+    protected function materialDesignIconicFontIcon($name, $size, $fixedWidth, $border, $pull, $spin, $rotate, $flip) {
+
+        // Initialize the values.
+        $sizes   = ["lg", "2x", "3x", "4x", "5x"];
+        $borders = ["border", "border-circle"];
+        $pulls   = ["left", "right"];
+        $spins   = ["spin", "spin-reverse"];
+        $rotates = ["90", "180", "270"];
+        $flips   = ["horizontal", "vertical"];
+
+        // Initialize the template.
+        $template = "<i %attributes%></i>";
+
+        // Initialize the attributes.
+        $attributes = [];
+
+        $attributes["class"][] = "zmdi";
+        $attributes["class"][] = null !== $name ? "zmdi-" . $name : null;
+        $attributes["class"][] = true === in_array($size, $sizes) ? "zmdi-hc-" . $size : null;
+        $attributes["class"][] = true === $fixedWidth ? "zmdi-hc-fw" : null;
+        $attributes["class"][] = true === in_array($border, $borders) ? "zmdi-hc-" . $border : null;
+        $attributes["class"][] = true === in_array($pull, $pulls) ? "pull-" . $pull : null;
+        $attributes["class"][] = true === in_array($spin, $spins) ? "zmdi-hc-" . $spin : null;
+        $attributes["class"][] = true === in_array($rotate, $rotates) ? "zmdi-hc-rotate-" . $rotate : null;
+        $attributes["class"][] = true === in_array($flip, $flips) ? "zmdi-hc-flip-" . $flip : null;
+
+        // Return the HTML.
+        return StringUtility::replace($template, ["%attributes%"], [StringUtility::parseArray($attributes)]);
+    }
+
 }
---
+++ Twig/Extension/Plugin/MaterialDesignIconicFontPluginTwigExtension.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Twig\Extension\Plugin;
+
+use Twig_SimpleFilter;
+use Twig_SimpleFunction;
+use WBW\Library\Core\Utility\Argument\ArrayUtility;
+use WBW\Library\Core\Utility\Argument\StringUtility;
+
+/**
+ * Material Design Iconic Font plugin Twig extension.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Twig\Extension\Plugin
+ */
+class MaterialDesignIconicFontPluginTwigExtension extends AbstractPluginTwigExtension {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "webeweb.bundle.bootstrapbundle.twig.extension.plugin.materialdesigniconicfont";
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Get the Twig filters.
+     *
+     * @return Twig_SimpleFilter[] Returns the Twig filters.
+     */
+    public function getFilters() {
+        return [
+            new Twig_SimpleFilter("materialDesignIconicFontList", [$this, "materialDesignIconicFontListFilter"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFilter("materialDesignIconicFontListIcon", [$this, "materialDesignIconicFontListIconFilter"], ["is_safe" => ["html"]]),
+        ];
+    }
+
+    /**
+     * Get the Twig functions.
+     *
+     * @return array Returns the Twig functions.
+     */
+    public function getFunctions() {
+        return [
+            new Twig_SimpleFunction("materialDesignIconicFontIcon", [$this, "materialDesignIconicFontIconFunction"], ["is_safe" => ["html"]]),
+        ];
+    }
+
+    /**
+     * Displays a Font Awesome.
+     *
+     * @param array $args The arguments.
+     * @return Returns a Font Awesome.
+     */
+    public function materialDesignIconicFontIconFunction(array $args = []) {
+        return $this->materialDesignIconicFontIcon(ArrayUtility::get($args, "name", "home"), ArrayUtility::get($args, "size"), ArrayUtility::get($args, "fixedWidth", false), ArrayUtility::get($args, "border", false), ArrayUtility::get($args, "pull"), ArrayUtility::get($args, "spin"), ArrayUtility::get($args, "rotate"), ArrayUtility::get($args, "flip"));
+    }
+
+    /**
+     * Displays a Material Design Iconic Font list.
+     *
+     * @param array|string $items The list items.
+     * @return string Returns the Material Design Iconic Font list.
+     */
+    public function materialDesignIconicFontListFilter($items) {
+
+        // Initialize the template.
+        $template = '<ul class="zmdi-hc-ul">%innerHTML%</ul>';
+
+        // Initialize the parameters.
+        $innerHTML = true === is_array($items) ? implode("\n", $items) : $items;
+
+        // Return the HTML.
+        return StringUtility::replace($template, ["%innerHTML%"], [$innerHTML]);
+    }
+
+    /**
+     * Displays a Material Design Iconic Font list icon.
+     *
+     * @param string $icon The Material Design Iconic Fonticon.
+     * @param string $content The content.
+     * @return string Returns the Material Design Iconic Font list icon.
+     */
+    public function materialDesignIconicFontListIconFilter($icon, $content) {
+
+        // Initialize the template.
+        $template = '<li>%glyphicon%%innerHTML%</li>';
+
+        // Initialize the parameters.
+        $glyphicon = null !== $icon ? StringUtility::replace($icon, ["class=\"zmdi"], ["class=\"zmdi-hc-li zmdi"]) : "";
+        $innerHTML = null !== $content ? $content : "";
+
+        // Return the HTML.
+        return StringUtility::replace($template, ["%glyphicon%", "%innerHTML%"], [$glyphicon, $innerHTML]);
+    }
+
+}
