81bd0545ba5de71736ba9eb275f396163fcefb76
webeweb <support@webeweb.fr>
2018-06-27 21:32:34 +0200

    Add Bad user role exception

---
+++ Exception/AbstractBootstrapException.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Exception;
+
+use Exception;
+
+/**
+ * Abstract Bootstrap exception.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Exception
+ * @abstract
+ */
+abstract class AbstractBootstrapException extends Exception {
+
+    /**
+     * Constructor.
+     *
+     * @param string $message The message.
+     * @param integer $code The code.
+     * @param Exception $previous The previous.
+     */
+    public function __construct($message, $code = 500, Exception $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }
+
+}
