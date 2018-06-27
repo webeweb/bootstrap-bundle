6812a90b4507612599fe8f79ce3d72ece9055389
webeweb <support@webeweb.fr>
2018-06-27 20:32:49 +0200

    Add User helper

---
+++ Helper/UserHelper.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Helper;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * User helper.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Helper
+ */
+class UserHelper {
+
+    /**
+     * Determines if the connected user entity has role $roles.
+     *
+     * @param object $user The user.
+     * @param string|array $roles The role or roles.
+     * @param boolean $or OR ? If true, matches a role cause a break and the method returns true.
+     * @return boolean Returns true in case of success, false otherwise.
+     */
+    public function hasRoles($user, $roles, $or = true) {
+
+        // Check the user.
+        if (null === $user || false === ($user instanceof UserInterface)) {
+            return false;
+        }
+
+        // Check the roles.
+        if (false === is_array($roles)) {
+            $roles = [$roles];
+        }
+
+        // Initialize the result.
+        $result = 1 <= count($roles);
+
+        // Handle each role.
+        foreach ($roles as $role) {
+            $result &= in_array($role, $user->getRoles());
+            if (true == $result && true === $or) {
+                break;
+            }
+        }
+
+        // Return the result.
+        return boolval($result);
+    }
+
+}
---
+++ Tests/Helper/UserHelperTest.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Helper;
+
+use Symfony\Component\Security\Core\User\User;
+use WBW\Bundle\BootstrapBundle\Helper\UserHelper;
+use WBW\Bundle\BootstrapBundle\Tests\Cases\AbstractBootstrapFrameworkTestCase;
+
+/**
+ * User helper test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Helper
+ */
+final class UserHelperTest extends AbstractBootstrapFrameworkTestCase {
+
+    /**
+     * Tests the hasRoles() method.
+     *
+     * @return void
+     */
+    public function testHasRoles() {
+
+        $arg = new User("github", "github", ["ROLE_SUPER_ADMIN", "ROLE_USER"]);
+
+        $this->assertTrue(UserHelper::hasRoles($arg, "ROLE_SUPER_ADMIN"));
+        $this->assertTrue(UserHelper::hasRoles($arg, "ROLE_SUPER_ADMIN", false));
+
+        $this->assertTrue(UserHelper::hasRoles($arg, "ROLE_USER"));
+        $this->assertTrue(UserHelper::hasRoles($arg, "ROLE_USER", false));
+
+        $this->assertFalse(UserHelper::hasRoles($arg, "ROLE_GITHUB"));
+        $this->assertFalse(UserHelper::hasRoles($arg, "ROLE_GITHUB", false));
+
+        $this->assertTrue(UserHelper::hasRoles($arg, ["ROLE_SUPER_ADMIN", "ROLE_USER"]));
+        $this->assertTrue(UserHelper::hasRoles($arg, ["ROLE_SUPER_ADMIN", "ROLE_USER"], false));
+
+        $this->assertTrue(UserHelper::hasRoles($arg, ["ROLE_SUPER_ADMIN", "ROLE_GITHUB"]));
+        $this->assertFalse(UserHelper::hasRoles($arg, ["ROLE_SUPER_ADMIN", "ROLE_GITHUB"], false));
+    }
+
+}
