cde99ffc5c269c20a1273e0fb0ee6542cfb2fa89
webeweb <support@webeweb.fr>
2018-07-18 21:13:14 +0200

    Reorganize Code Twig extension

---
+++ Tests/Twig/Extension/CSS/CodeTwigExtensionTest.php
@@ -0,0 +1,162 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\CSS;
+
+use Twig_Node;
+use Twig_SimpleFunction;
+use WBW\Bundle\BootstrapBundle\Tests\Cases\AbstractBootstrapFrameworkTestCase;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\CSS\CodeTwigExtension;
+
+/**
+ * Code Twig extension test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\CSS
+ * @final
+ */
+final class CodeTwigExtensionTest extends AbstractBootstrapFrameworkTestCase {
+
+    /**
+     * Tests the getFunctions() method.
+     *
+     * @return void
+     */
+    public function testGetFunctions() {
+
+        $obj = new CodeTwigExtension();
+
+        $res = $obj->getFunctions();
+
+        $this->assertCount(5, $res);
+
+        $this->assertInstanceOf(Twig_SimpleFunction::class, $res[0]);
+        $this->assertEquals("bootstrapBasicBlock", $res[0]->getName());
+        $this->assertEquals([$obj, "bootstrapBasicBlockFunction"], $res[0]->getCallable());
+        $this->assertEquals(["html"], $res[0]->getSafe(new Twig_Node()));
+
+        $this->assertInstanceOf(Twig_SimpleFunction::class, $res[1]);
+        $this->assertEquals("bootstrapInline", $res[1]->getName());
+        $this->assertEquals([$obj, "bootstrapInlineFunction"], $res[1]->getCallable());
+        $this->assertEquals(["html"], $res[1]->getSafe(new Twig_Node()));
+
+        $this->assertInstanceOf(Twig_SimpleFunction::class, $res[2]);
+        $this->assertEquals("bootstrapSampleOutput", $res[2]->getName());
+        $this->assertEquals([$obj, "bootstrapSampleOutputFunction"], $res[2]->getCallable());
+        $this->assertEquals(["html"], $res[2]->getSafe(new Twig_Node()));
+
+        $this->assertInstanceOf(Twig_SimpleFunction::class, $res[3]);
+        $this->assertEquals("bootstrapUserInput", $res[3]->getName());
+        $this->assertEquals([$obj, "bootstrapUserInputFunction"], $res[3]->getCallable());
+        $this->assertEquals(["html"], $res[3]->getSafe(new Twig_Node()));
+
+        $this->assertInstanceOf(Twig_SimpleFunction::class, $res[4]);
+        $this->assertEquals("bootstrapVariable", $res[4]->getName());
+        $this->assertEquals([$obj, "bootstrapVariableFunction"], $res[4]->getCallable());
+        $this->assertEquals(["html"], $res[4]->getSafe(new Twig_Node()));
+    }
+
+    /**
+     * Tests the bootstrapBasicBlockFunction() method.
+     *
+     * @return void
+     * @depends testGetFunctions
+     */
+    public function testBootstrapBasicBlockFunction() {
+
+        $obj = new CodeTwigExtension();
+
+        $arg0 = [];
+        $res0 = "<pre></pre>";
+        $this->assertEquals($res0, $obj->bootstrapBasicBlockFunction($arg0));
+
+        $arg9 = ["content" => "content"];
+        $res9 = "<pre>content</pre>";
+        $this->assertEquals($res9, $obj->bootstrapBasicBlockFunction($arg9));
+    }
+
+    /**
+     * Tests the bootstrapInlineFunction() method.
+     *
+     * @return void
+     * @depends testGetFunctions
+     */
+    public function testBootstrapInlineFunction() {
+
+        $obj = new CodeTwigExtension();
+
+        $arg0 = [];
+        $res0 = "<code></code>";
+        $this->assertEquals($res0, $obj->bootstrapInlineFunction($arg0));
+
+        $arg9 = ["content" => "content"];
+        $res9 = "<code>content</code>";
+        $this->assertEquals($res9, $obj->bootstrapInlineFunction($arg9));
+    }
+
+    /**
+     * Tests the bootstrapSampleOutputFunction() method.
+     *
+     * @return void
+     * @depends testGetFunctions
+     */
+    public function testBootstrapSampleOutputFunction() {
+
+        $obj = new CodeTwigExtension();
+
+        $arg0 = [];
+        $res0 = "<samp></samp>";
+        $this->assertEquals($res0, $obj->bootstrapSampleOutputFunction($arg0));
+
+        $arg9 = ["content" => "content"];
+        $res9 = "<samp>content</samp>";
+        $this->assertEquals($res9, $obj->bootstrapSampleOutputFunction($arg9));
+    }
+
+    /**
+     * Tests the bootstrapUserInputFunction() method.
+     *
+     * @return void
+     * @depends testGetFunctions
+     */
+    public function testBootstrapUserInputFunction() {
+
+        $obj = new CodeTwigExtension();
+
+        $arg0 = [];
+        $res0 = "<kbd></kbd>";
+        $this->assertEquals($res0, $obj->bootstrapUserInputFunction($arg0));
+
+        $arg9 = ["content" => "content"];
+        $res9 = "<kbd>content</kbd>";
+        $this->assertEquals($res9, $obj->bootstrapUserInputFunction($arg9));
+    }
+
+    /**
+     * Tests the bootstrapVariableFunction() method.
+     *
+     * @return void
+     * @depends testGetFunctions
+     */
+    public function testBootstrapVariableFunction() {
+
+        $obj = new CodeTwigExtension();
+
+        $arg0 = [];
+        $res0 = "<var></var>";
+        $this->assertEquals($res0, $obj->bootstrapVariableFunction($arg0));
+
+        $arg9 = ["content" => "content"];
+        $res9 = "<var>content</var>";
+        $this->assertEquals($res9, $obj->bootstrapVariableFunction($arg9));
+    }
+
+}
---
+++ Twig/Extension/CSS/CodeTwigExtension.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Twig\Extension\CSS;
+
+use Twig_SimpleFunction;
+use WBW\Library\Core\Utility\Argument\ArrayUtility;
+
+/**
+ * Code Twig extension.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Twig\Extension\CSS
+ * @see https://getbootstrap.com/docs/3.3/css/#code
+ */
+class CodeTwigExtension extends AbstractCodeTwigExtension {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "webeweb.bootstrapbundle.twig.extension.css.code";
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Displays a Bootstrap basic block.
+     *
+     * @param array $args The arguments.
+     * @return string Returns the Bootstrap basic block.
+     */
+    public function bootstrapBasicBlockFunction(array $args = []) {
+        return $this->bootstrapBasicBlock(ArrayUtility::get($args, "content"));
+    }
+
+    /**
+     * Displays a Bootstrap inline.
+     *
+     * @param array $args The arguments.
+     * @return string Returns the Bootstrap inline.
+     */
+    public function bootstrapInlineFunction(array $args = []) {
+        return $this->bootstrapInline(ArrayUtility::get($args, "content"));
+    }
+
+    /**
+     * Displays a Bootstrap inline.
+     *
+     * @param array $args The arguments.
+     * @return string Returns the Bootstrap inline.
+     */
+    public function bootstrapSampleOutputFunction(array $args = []) {
+        return $this->bootstrapSampleOutput(ArrayUtility::get($args, "content"));
+    }
+
+    /**
+     * Displays a Bootstrap user input.
+     *
+     * @param array $args The arguments.
+     * @return string Returns the Bootstrap user input.
+     */
+    public function bootstrapUserInputFunction(array $args = []) {
+        return $this->bootstrapUserInput(ArrayUtility::get($args, "content"));
+    }
+
+    /**
+     * Displays a Bootstrap variable.
+     *
+     * @param array $args The arguments.
+     * @return string Returns the Bootstrap variable.
+     */
+    public function bootstrapVariableFunction(array $args = []) {
+        return $this->bootstrapVariable(ArrayUtility::get($args, "content"));
+    }
+
+    /**
+     * Get the Twig functions.
+     *
+     * @return array Returns the Twig functions.
+     */
+    public function getFunctions() {
+        return [
+            new Twig_SimpleFunction("bootstrapBasicBlock", [$this, "bootstrapBasicBlockFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("bootstrapInline", [$this, "bootstrapInlineFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("bootstrapSampleOutput", [$this, "bootstrapSampleOutputFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("bootstrapUserInput", [$this, "bootstrapUserInputFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("bootstrapVariable", [$this, "bootstrapVariableFunction"], ["is_safe" => ["html"]]),
+        ];
+    }
+
+}
