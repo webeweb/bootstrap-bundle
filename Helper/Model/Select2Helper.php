83afbc607d36319321e1aedf9939fea05a71aac1
webeweb <support@webeweb.fr>
2018-08-17 11:20:41 +0200

    Add Select2 helper (use to provide "results" array)

---
+++ Helper/Model/Select2Helper.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Helper\Model;
+
+use WBW\Bundle\BootstrapBundle\Model\Select2\Select2DataFormatInterface;
+use WBW\Library\Core\Exception\Argument\IllegalArgumentException;
+
+/**
+ * Select2 helper.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package  WBW\Bundle\BootstrapBundle\Helper\Model
+ */
+class Select2Helper {
+
+    /**
+     * Convert items into a "results" array.
+     *
+     * @param Select2DataFormatInterface[] $items The items.
+     * @return array Returns the "results" array.
+     * @throws IllegalArgumentException Throws an illegal argument exceptino if an item does not implement Select2DataFormatInterface.
+     */
+    public static function toResults($items) {
+
+        // Initialize the output.
+        $output = [];
+
+        // Handle each item.
+        foreach ($items as $current) {
+
+            // Check the item.
+            if (false === ($current instanceof Select2DataFormatInterface)) {
+                throw new IllegalArgumentException("The item must implements Select2DataFormatInterface");
+            }
+
+            // Add the item.
+            $output[] = [
+                "id"   => $current->getSelect2DataFormatId(),
+                "text" => $current->getSelect2DataFormatText(),
+            ];
+        }
+
+        // Return the output.
+        return $output;
+    }
+
+}
---
+++ Tests/Helper/Model/Select2HelperTest.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Helper\Model;
+
+use Exception;
+use WBW\Bundle\BootstrapBundle\Helper\Model\Select2Helper;
+use WBW\Bundle\BootstrapBundle\Navigation\NavigationNode;
+use WBW\Bundle\BootstrapBundle\Tests\Cases\AbstractBootstrapFrameworkTestCase;
+use WBW\Bundle\BootstrapBundle\Tests\Fixtures\Model\Select2\Select2Item;
+use WBW\Library\Core\Exception\Argument\IllegalArgumentException;
+
+/**
+ * Select2 helper test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Helper\Model
+ * @final
+ */
+final class Select2HelperTest extends AbstractBootstrapFrameworkTestCase {
+
+    /**
+     * Items.
+     *
+     * @var array
+     */
+    private $items;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp() {
+        parent::setUp();
+
+        // Set a items array mock.
+        $this->items = [];
+
+        $this->items[] = new Select2Item("1", "item1");
+        $this->items[] = new Select2Item("2", "item2");
+        $this->items[] = new Select2Item("3", "item3");
+    }
+
+    /**
+     * Tests the toResults() method.
+     *
+     * @retrurn void
+     */
+    public function testToResults() {
+
+        $res = [
+            [
+                "id"   => "1",
+                "text" => "item1",
+            ],
+            [
+                "id"   => "2",
+                "text" => "item2",
+            ],
+            [
+                "id"   => "3",
+                "text" => "item3",
+            ],
+        ];
+        $this->assertEquals($res, Select2Helper::toResults($this->items));
+
+        try {
+
+            $this->items[] = new NavigationNode("id4");
+            Select2Helper::toResults($this->items);
+        } catch (Exception $ex) {
+
+            $this->assertInstanceOf(IllegalArgumentException::class, $ex);
+            $this->assertEquals("The item must implements Select2DataFormatInterface", $ex->getMessage());
+        }
+    }
+
+}
