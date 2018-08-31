503c1d6bfe622ca0e4d8f6aabf9d40933233005a
webeweb <support@webeweb.fr>
2018-08-31 17:50:06 +0200

    Add Bootstrap manager interface

---
+++ Manager/ManagerInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Manager;
+
+use WBW\Bundle\BootstrapBundle\Provider\ProviderInterface;
+
+/**
+ * Manager interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Manager
+ */
+interface ManagerInterface {
+
+    /**
+     * Register a provider.
+     *
+     * @param ProviderInterface $provider The provider
+     * @return void
+     */
+    public function registerProvider(ProviderInterface $provider);
+}
