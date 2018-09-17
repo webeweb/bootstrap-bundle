a617ce539acc0da83a85849a478ffe99143bb824
webeweb <support@webeweb.fr>
2018-09-17 10:51:20 +0200

    Update TestCase inheritance

--- Tests/Twig/Extension/Component/BreadcrumbTwigExtensionTest.php
+++ Tests/Twig/Extension/Component/BreadcrumbTwigExtensionTest.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Twig_Node;
 use Twig_SimpleFunction;
 use WBW\Bundle\BootstrapBundle\Navigation\NavigationTree;
-use WBW\Bundle\BootstrapBundle\Tests\Cases\AbstractBootstrapFrameworkTestCase;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractBootstrapFrameworkTestCase;
 use WBW\Bundle\BootstrapBundle\Tests\Fixtures\App\TestFixtures;
 use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\BreadcrumbTwigExtension;
 
