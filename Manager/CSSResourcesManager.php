e88a97da10e805a8f6d7aa6850c86fc1a9dbaaf4
webeweb <support@webeweb.fr>
2018-09-17 14:21:58 +0200

    Create a CSS resources manager

---
+++ Manager/CSSResourcesManager.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Manager;
+
+/**
+ * CSS resources manager.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Manager
+ */
+class CSSResourcesManager extends AbstractResourcesManager {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "webeweb.bootstrap.manager.resources.css";
+
+    /**
+     * Constructor.
+     *
+     * @param string $directory The directory.
+     */
+    public function __construct($directory) {
+        parent::__construct($directory, ".css");
+    }
+
+}
---
+++ Tests/Manager/CSSResourcesManagerTest.php
@@ -0,0 +1,109 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Manager;
+
+use WBW\Bundle\BootstrapBundle\Manager\CSSResourcesManager;
+use WBW\Bundle\BootstrapBundle\Provider\Resources\CSSResourcesProvider;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractBootstrapFrameworkTestCase;
+
+/**
+ * CSS resources manager test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Manager
+ * @final
+ */
+final class CSSResourcesManagerTest extends AbstractBootstrapFrameworkTestCase {
+
+    /**
+     * Directory.
+     *
+     * @var string
+     */
+    private $directory;
+
+    /**
+     * Provider.
+     *
+     * @var CSSResourcesProvider
+     */
+    private $provider;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp() {
+        parent::setUp();
+
+        // Set a Directory mock.
+        $this->directory = getcwd() . "/Tests/Fixtures/App/web";
+
+        // Set a CSS resources provider mock.
+        $this->provider = new CSSResourcesProvider();
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new CSSResourcesManager($this->directory);
+
+        $this->assertEquals($this->directory, $obj->getDirectory());
+        $this->assertEquals(".css", $obj->getExtension());
+        $this->assertRegExp("/^(.*)\/resources.css$/", $obj->getFilename());
+        $this->assertEquals([], $obj->getProviders());
+    }
+
+    /**
+     * Tests the addProvider() method.
+     *
+     * @return void
+     */
+    public function testAddProvider() {
+
+        $obj = new CSSResourcesManager($this->directory);
+
+        $this->assertSame($obj, $obj->addProvider($this->provider));
+        $this->assertCount(1, $obj->getProviders());
+        $this->assertSame($this->provider, $obj->getProviders()[0]);
+    }
+
+    /**
+     * Tests the exists() method.
+     *
+     * @return void
+     */
+    public function testExists() {
+
+        $obj = new CSSResourcesManager($this->directory);
+
+        $this->assertFalse($obj->exists());
+    }
+
+    /**
+     * Tests the registerProvider() method.
+     *
+     * @return void
+     */
+    public function testRegisterProvider() {
+
+        $obj = new CSSResourcesManager($this->directory);
+
+        $this->assertSame($obj, $obj->registerProvider($this->provider));
+        $this->assertCount(1, $obj->getProviders());
+        $this->assertSame($this->provider, $obj->getProviders()[0]);
+    }
+
+}
