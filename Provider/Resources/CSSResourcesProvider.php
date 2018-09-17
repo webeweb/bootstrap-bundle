6f149d64df93a9d3799a0191e8d7ac4259ad57c7
webeweb <support@webeweb.fr>
2018-09-17 17:52:19 +0200

    Update CSS resources provider inheritance

--- Provider/Resources/CSSResourcesProvider.php
+++ Provider/Resources/CSSResourcesProvider.php
@@ -17,7 +17,7 @@ namespace WBW\Bundle\BootstrapBundle\Provider\Resources;
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\BootstrapBundle\Provider\Resources
  */
-class CSSResourcesProvider implements CSSResourcesProviderInterface {
+class CSSResourcesProvider extends AbstractResourcesProvider implements CSSResourcesProviderInterface {
 
     /**
      * {@inheritdoc}
@@ -28,43 +28,43 @@ class CSSResourcesProvider implements CSSResourcesProviderInterface {
         $output = [];
 
         // Animate.css
-        $output[] = "animate.css-3.5.2/animate.min.css";
+        $output[] = "/animate.css-3.5.2/animate.min.css";
         // Bootstrap
-        $output[] = "bootstrap-3.3.7/css/bootstrap.min.css";
+        $output[] = "/bootstrap-3.3.7/css/bootstrap.min.css";
         // Bootstrap Colorpicker plug-in
-        $output[] = "bootstrap-colorpicker-2.5.1/css/bootstrap-colorpicker.min.css";
+        $output[] = "/bootstrap-colorpicker-2.5.1/css/bootstrap-colorpicker.min.css";
         // Bootstrap Datepicker plug-in
-        $output[] = "bootstrap-datepicker-1.7.1/css/bootstrap-datepicker.min.css";
+        $output[] = "/bootstrap-datepicker-1.7.1/css/bootstrap-datepicker.min.css";
         // Bootstrap Daterangepicker plugin
-        $output[] = "bootstrap-daterangepicker-2.1.27/daterangepicker.css";
+        $output[] = "/bootstrap-daterangepicker-2.1.27/daterangepicker.css";
         // Bootstrap Markdown plug-in
-        $output[] = "bootstrap-markdown-2.10.0/css/bootstrap-markdown.min.css";
+        $output[] = "/bootstrap-markdown-2.10.0/css/bootstrap-markdown.min.css";
         // Bootstrap Select plug-in
-        $output[] = "bootstrap-select-1.12.4/css/bootstrap-select.min.css";
+        $output[] = "/bootstrap-select-1.12.4/css/bootstrap-select.min.css";
         // Bootstrap Slider plug-in
-        $output[] = "bootstrap-slider-10.0.0/css/bootstrap-slider.min.css";
+        $output[] = "/bootstrap-slider-10.0.0/css/bootstrap-slider.min.css";
         // Bootstrap Social plug-in
-        $output[] = "bootstrap-social-5.1.1/bootstrap-social.css";
+        $output[] = "/bootstrap-social-5.1.1/bootstrap-social.css";
         // Bootstrap Tagsinput plug-in
-        $output[] = "bootstrap-tagsinput-0.8.0/bootstrap-tagsinput.css";
+        $output[] = "/bootstrap-tagsinput-0.8.0/bootstrap-tagsinput.css";
         // Bootstrap Timepicker plug-in
-        $output[] = "bootstrap-timepicker-0.5.2/css/bootstrap-timepicker.min.css";
+        $output[] = "/bootstrap-timepicker-0.5.2/css/bootstrap-timepicker.min.css";
         // Bootstrap WYSIWIG plug-in
-        $output[] = "bootstrap-wysiwyg-0.3.3/bootstrap3-wysihtml5.min.css";
+        $output[] = "/bootstrap-wysiwyg-0.3.3/bootstrap3-wysihtml5.min.css";
         // Font Awesome
-        $output[] = "fontawesome-5.0.10/css/fontawesome-all.min.css";
+        $output[] = "/fontawesome-5.0.10/css/fontawesome-all.min.css";
         // jQuery Select2 plug-in
-        $output[] = "jquery-select2-4.0.5/css/select2.min.css";
+        $output[] = "/jquery-select2-4.0.5/css/select2.min.css";
         // Material Design Color Palette
-        $output[] = "material-design-color-palette-1.1.0/material-design-color-palette.min.css";
+        $output[] = "/material-design-color-palette-1.1.0/material-design-color-palette.min.css";
         // Material Design Hierarchical Display
-        $output[] = "material-design-hierarchical-display-1.0.1/zmd.hierarchical-display.min.css";
+        $output[] = "/material-design-hierarchical-display-1.0.1/zmd.hierarchical-display.min.css";
         // Material Design Iconic Font
-        $output[] = "material-design-iconic-font-2.2.0/css/material-design-iconic-font.min.css";
+        $output[] = "/material-design-iconic-font-2.2.0/css/material-design-iconic-font.min.css";
         // Meteocons
-        $output[] = "meteocons/stylesheet.css";
+        $output[] = "/meteocons/stylesheet.css";
         // waitMe
-        $output[] = "waitme-1.19/waitMe.min.css";
+        $output[] = "/waitme-1.19/waitMe.min.css";
 
         // Return the ouput.
         return $output;
