c2acffd980a34f7c13dac773f51d56711ee996ba
webeweb <support@webeweb.fr>
2018-09-17 18:05:37 +0200

    Create a TestResourceProvider to throw an illegal argument exception

---
+++ Tests/Fixtures/Provider/Resources/TestResourcesProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Fixtures\Provider\Resources;
+
+use WBW\Bundle\BootstrapBundle\BootstrapBundle;
+use WBW\Bundle\BootstrapBundle\Provider\Resources\CSSResourcesProviderInterface;
+
+/**
+ * Test resources provider.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Fixtures\Provider\Resources
+ * @final
+ */
+final class TestResourcesProvider implements CSSResourcesProviderInterface {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDirectory() {
+        return BootstrapBundle::getResourcesDirectory() . "/public";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getResources() {
+
+        // Initialize the resources.
+        $resources = [];
+
+        $resources[] = "/animate.css-3.5.2/animate.min.css";
+        $resources[] = "/animate.css-3.5.2/animate.min.js";
+
+        // Returns the resources.
+        return $resources;
+    }
+
+}
