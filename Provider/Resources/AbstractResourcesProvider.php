a0003ae42e52d60bf09f93967a5a3a7fcb2ad4c1
webeweb <support@webeweb.fr>
2018-09-17 17:51:33 +0200

    Create an abstract resources provider

---
+++ Provider/Resources/AbstractResourcesProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Provider\Resources;
+
+use WBW\Bundle\BootstrapBundle\BootstrapBundle;
+use WBW\Bundle\BootstrapBundle\Provider\ResourcesProviderInterface;
+
+/**
+ * Abstract resources provider.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Provider\Resources
+ * @abstract
+ */
+abstract class AbstractResourcesProvider implements ResourcesProviderInterface {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO.
+    }
+
+    /**
+     * Get the directory.
+     *
+     * @return string Returns the directory.
+     */
+    public function getDirectory() {
+        return BootstrapBundle::getResourcesDirectory() . "/public";
+    }
+
+}
