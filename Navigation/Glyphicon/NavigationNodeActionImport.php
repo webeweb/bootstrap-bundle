3c51dd507f1985364a7cd259de0db67482c6791f
webeweb <support@webeweb.fr>
2018-11-21 18:06:54 +0100

    Add Navigation node action "import" (with Glyphicon icon)

--- Navigation/Glyphicon/NavigationNodeActionImport.php
+++ Navigation/Glyphicon/NavigationNodeActionImport.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of the adminbsb-material-design-bundle package.
+ * This file is part of the bootstrap-bundle package.
  *
  * (c) 2018 WEBEWEB
  *
@@ -9,17 +9,17 @@
  * file that was distributed with this source code.
  */
 
-namespace WBW\Bundle\BootstrapBundle\Navigation\Predefined;
+namespace WBW\Bundle\BootstrapBundle\Navigation\Glyphicon;
 
 use WBW\Bundle\CoreBundle\Navigation\NavigationNode;
 
 /**
- * Navigation node "Import" action.
+ * Navigation node action "Import".
  *
  * @author webeweb <https://github.com/webeweb/>
- * @package WBW\Bundle\BootstrapBundle\Navigation\Predefined
+ * @package WBW\Bundle\BootstrapBundle\Navigation\Glyphicon
  */
-class NavigationNodeImportAction extends NavigationNode {
+class NavigationNodeActionImport extends NavigationNode {
 
     /**
      * Constructor.
--- Tests/Navigation/Glyphicon/NavigationNodeActionImportTest.php
+++ Tests/Navigation/Glyphicon/NavigationNodeActionImportTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of the adminbsb-material-design-bundle package.
+ * This file is part of the core-bundle package.
  *
  * (c) 2018 WEBEWEB
  *
@@ -9,19 +9,19 @@
  * file that was distributed with this source code.
  */
 
-namespace WBW\Bundle\BootstrapBundle\Tests\Model\Navigation\Predefined;
+namespace WBW\Bundle\BootstrapBundle\Tests\Navigation\Glyphicon;
 
-use WBW\Bundle\BootstrapBundle\Navigation\NavigationInterface;
-use WBW\Bundle\BootstrapBundle\Navigation\Predefined\NavigationNodeImportAction;
+use WBW\Bundle\BootstrapBundle\Navigation\Glyphicon\NavigationNodeActionImport;
 use WBW\Bundle\BootstrapBundle\Tests\AbstractFrameworkTestCase;
+use WBW\Bundle\CoreBundle\Navigation\NavigationInterface;
 
 /**
- * Navigation node "Import" action test.
+ * Navigation node action "Import" test.
  *
  * @author webeweb <https://github.com/webeweb/>
- * @package WBW\Bundle\BootstrapBundle\Tests\Model\Navigation\Predefined
+ * @package WBW\Bundle\BootstrapBundle\Tests\Navigation\Glyphicon
  */
-class NavigationNodeImportActionTest extends AbstractFrameworkTestCase {
+class NavigationNodeActionImportTest extends AbstractFrameworkTestCase {
 
     /**
      * Tests the __construct() method.
@@ -30,12 +30,12 @@ class NavigationNodeImportActionTest extends AbstractFrameworkTestCase {
      */
     public function testConstruct() {
 
-        $obj = new NavigationNodeImportAction("route");
+        $obj = new NavigationNodeActionImport("route");
 
         $this->assertEquals("navigation.node.action.import", $obj->getId());
         $this->assertEquals("g:save", $obj->getIcon());
         $this->assertEquals(NavigationInterface::NAVIGATION_MATCHER_URL, $obj->getMatcher());
-        $this->assertEquals("route", $obj->getRoute());
+        $this->assertEquals("route", $obj->getUri());
     }
 
 }
