6f3ff2762e011dc30e0966f57afe6acd95a87569
webeweb <support@webeweb.fr>
2018-11-21 18:05:27 +0100

    Add Breadcrumb node action "new user" (with Glyphicon icon)

--- Tests/Navigation/Glyphicon/BreadcrumbNodeActionNewUserTest.php
+++ Tests/Navigation/Glyphicon/BreadcrumbNodeActionNewUserTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of the adminbsb-material-design-bundle package.
+ * This file is part of the core-bundle package.
  *
  * (c) 2018 WEBEWEB
  *
@@ -9,19 +9,19 @@
  * file that was distributed with this source code.
  */
 
-namespace WBW\Bundle\BootstrapBundle\Tests\Model\Navigation\Predefined;
+namespace WBW\Bundle\BootstrapBundle\Tests\Navigation\Glyphicon;
 
-use WBW\Bundle\BootstrapBundle\Navigation\NavigationInterface;
-use WBW\Bundle\BootstrapBundle\Navigation\Predefined\BreadcrumbNodeNewUserAction;
+use WBW\Bundle\BootstrapBundle\Navigation\Glyphicon\BreadcrumbNodeActionNewUser;
 use WBW\Bundle\BootstrapBundle\Tests\AbstractFrameworkTestCase;
+use WBW\Bundle\CoreBundle\Navigation\NavigationInterface;
 
 /**
- * Breadcrumb node "New user" action test.
+ * Breadcrumb node action "New user" test.
  *
  * @author webeweb <https://github.com/webeweb/>
- * @package WBW\Bundle\BootstrapBundle\Tests\Model\Navigation\Predefined
+ * @package WBW\Bundle\BootstrapBundle\Tests\Navigation\Glyphicon
  */
-class BreadcrumbNodeNewUserActionTest extends AbstractFrameworkTestCase {
+class BreadcrumbNodeActionNewUserTest extends AbstractFrameworkTestCase {
 
     /**
      * Tests the __construct() method.
@@ -30,12 +30,12 @@ class BreadcrumbNodeNewUserActionTest extends AbstractFrameworkTestCase {
      */
     public function testConstruct() {
 
-        $obj = new BreadcrumbNodeNewUserAction("route");
+        $obj = new BreadcrumbNodeActionNewUser("route");
 
         $this->assertEquals("navigation.node.action.new", $obj->getId());
         $this->assertEquals("g:plus", $obj->getIcon());
         $this->assertEquals(NavigationInterface::NAVIGATION_MATCHER_URL, $obj->getMatcher());
-        $this->assertEquals("route", $obj->getRoute());
+        $this->assertEquals("route", $obj->getUri());
     }
 
 }
