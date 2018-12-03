3923dd07fa76d6c5ae94bae3d9ee48e28d700c0b
webeweb <support@webeweb.fr>
2018-12-03 18:33:57 +0100

    Add Typography Twig extension trait

---
+++ Tests/Twig/Extension/CSS/TypographyTwigExtensionTraitTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\CSS;
+
+use WBW\Bundle\BootstrapBundle\Tests\Fixtures\Twig\Extension\CSS\TestTypographyTwigExtensionTrait;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\CSS\TypographyTwigExtension;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+
+/**
+ * Typography Twig extension trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\CSS
+ */
+class TypographyTwigExtensionTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TestTypographyTwigExtensionTrait();
+
+        $this->assertNull($obj->getTypographyTwigExtension());
+    }
+
+    /**
+     * Tests the setTypographyTwigExtension() method.
+     *
+     * @return void
+     */
+    public function testSetTypographyTwigExtension() {
+
+        // Set a Typography Twig extension mock.
+        $typographyTwigExtension = new TypographyTwigExtension($this->twigEnvironment);
+
+        $obj = new TestTypographyTwigExtensionTrait();
+
+        $obj->setTypographyTwigExtension($typographyTwigExtension);
+        $this->assertSame($typographyTwigExtension, $obj->getTypographyTwigExtension());
+    }
+
+}
---
+++ Twig/Extension/CSS/TypographyTwigExtensionTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Twig\Extension\CSS;
+
+/**
+ * Typography Twig extension trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Twig\Extension\CSS
+ */
+trait TypographyTwigExtensionTrait {
+
+    /**
+     * Typography Twig extension.
+     *
+     * @var TypographyTwigExtension
+     */
+    private $typographyTwigExtension;
+
+    /**
+     * Get the typography Twig extention.
+     *
+     * @return TypographyTwigExtension Returns the typography Twig extension.
+     */
+    public function getTypographyTwigExtension() {
+        return $this->typographyTwigExtension;
+    }
+
+    /**
+     * Set the typography Twig extension.
+     *
+     * @param TypographyTwigExtension $typographyTwigExtension The typography Twig extension.
+     */
+    protected function setTypographyTwigExtension(TypographyTwigExtension $typographyTwigExtension = null) {
+        $this->typographyTwigExtension = $typographyTwigExtension;
+        return $this;
+    }
+
+}
