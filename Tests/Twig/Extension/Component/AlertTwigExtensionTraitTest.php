e9cd0786d95fea7ed64c5d8762f7368965f12e9e
webeweb <support@webeweb.fr>
2018-12-03 18:34:19 +0100

    Add Alert Twig extension trait

---
+++ Tests/Twig/Extension/Component/AlertTwigExtensionTraitTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Component;
+
+use WBW\Bundle\BootstrapBundle\Tests\Fixtures\Twig\Extension\Component\TestAlertTwigExtensionTrait;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\AlertTwigExtension;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+
+/**
+ * Alert Twig extension trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Component
+ */
+class AlertTwigExtensionTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TestAlertTwigExtensionTrait();
+
+        $this->assertNull($obj->getAlertTwigExtension());
+    }
+
+    /**
+     * Tests the setAlertTwigExtension() method.
+     *
+     * @return void
+     */
+    public function testSetAlertTwigExtension() {
+
+        // Set an Alert Twig extension mock.
+        $alertTwigExtension = new AlertTwigExtension($this->twigEnvironment);
+
+        $obj = new TestAlertTwigExtensionTrait();
+
+        $obj->setAlertTwigExtension($alertTwigExtension);
+        $this->assertSame($alertTwigExtension, $obj->getAlertTwigExtension());
+    }
+
+}
---
+++ Twig/Extension/Component/AlertTwigExtensionTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Twig\Extension\Component;
+
+/**
+ * Alert Twig extension trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Twig\Extension\Component
+ */
+trait AlertTwigExtensionTrait {
+
+    /**
+     * Alert Twig extension.
+     *
+     * @var AlertTwigExtension
+     */
+    private $alertTwigExtension;
+
+    /**
+     * Get the alert Twig extention.
+     *
+     * @return AlertTwigExtension Returns the alert Twig extension.
+     */
+    public function getAlertTwigExtension() {
+        return $this->alertTwigExtension;
+    }
+
+    /**
+     * Set the alert Twig extension.
+     *
+     * @param AlertTwigExtension $alertTwigExtension The alert Twig extension.
+     */
+    protected function setAlertTwigExtension(AlertTwigExtension $alertTwigExtension = null) {
+        $this->alertTwigExtension = $alertTwigExtension;
+        return $this;
+    }
+
+}
