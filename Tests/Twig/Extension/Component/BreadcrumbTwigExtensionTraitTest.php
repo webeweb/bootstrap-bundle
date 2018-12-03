b7c1fb9c89b89304a64f25d2633c18ff33b81880
webeweb <support@webeweb.fr>
2018-12-03 18:34:42 +0100

    Add Breadcrumb Twig extension trait

---
+++ Tests/Twig/Extension/Component/BreadcrumbTwigExtensionTraitTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Component;
+
+use WBW\Bundle\BootstrapBundle\Tests\Fixtures\Twig\Extension\Component\TestBreadcrumbTwigExtensionTrait;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\BreadcrumbTwigExtension;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+
+/**
+ * Breadcrumb Twig extension trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Component
+ */
+class BreadcrumbTwigExtensionTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TestBreadcrumbTwigExtensionTrait();
+
+        $this->assertNull($obj->getBreadcrumbTwigExtension());
+    }
+
+    /**
+     * Tests the setBreadcrumbTwigExtension() method.
+     *
+     * @return void
+     */
+    public function testSetBreadcrumbTwigExtension() {
+
+        // Set a Breadcrumb Twig extension mock.
+        $breadcrumbTwigExtension = new BreadcrumbTwigExtension($this->twigEnvironment, $this->translator);
+
+        $obj = new TestBreadcrumbTwigExtensionTrait();
+
+        $obj->setBreadcrumbTwigExtension($breadcrumbTwigExtension);
+        $this->assertSame($breadcrumbTwigExtension, $obj->getBreadcrumbTwigExtension());
+    }
+
+}
---
+++ Twig/Extension/Component/BreadcrumbTwigExtensionTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Twig\Extension\Component;
+
+/**
+ * Breadcrumb Twig extension trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Twig\Extension\Component
+ */
+trait BreadcrumbTwigExtensionTrait {
+
+    /**
+     * Breadcrumb Twig extension.
+     *
+     * @var BreadcrumbTwigExtension
+     */
+    private $breadcrumbTwigExtension;
+
+    /**
+     * Get the breadcrumb Twig extention.
+     *
+     * @return BreadcrumbTwigExtension Returns the breadcrumb Twig extension.
+     */
+    public function getBreadcrumbTwigExtension() {
+        return $this->breadcrumbTwigExtension;
+    }
+
+    /**
+     * Set the breadcrumb Twig extension.
+     *
+     * @param BreadcrumbTwigExtension $breadcrumbTwigExtension The breadcrumb Twig extension.
+     */
+    protected function setBreadcrumbTwigExtension(BreadcrumbTwigExtension $breadcrumbTwigExtension = null) {
+        $this->breadcrumbTwigExtension = $breadcrumbTwigExtension;
+        return $this;
+    }
+
+}
