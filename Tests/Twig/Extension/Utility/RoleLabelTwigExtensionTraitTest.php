75a1f42b62e04aa5f4c8620875d4115594d8e068
webeweb <support@webeweb.fr>
2018-12-03 18:36:26 +0100

    Add Role label Twig extension trait

---
+++ Tests/Twig/Extension/Utility/RoleLabelTwigExtensionTraitTest.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Component;
+
+use WBW\Bundle\BootstrapBundle\Tests\Fixtures\Twig\Extension\Utility\TestRoleLabelTwigExtensionTrait;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\LabelTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Utility\RoleLabelTwigExtension;
+use WBW\Bundle\SyntaxHighlighterBundle\Tests\AbstractTestCase;
+
+/**
+ * Role label Twig extension trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\CSS
+ */
+class RoleLabelTwigExtensionTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TestRoleLabelTwigExtensionTrait();
+
+        $this->assertNull($obj->getRoleLabelTwigExtension());
+    }
+
+    /**
+     * Tests the setRoleLabelTwigExtension() method.
+     *
+     * @return void
+     */
+    public function testSetRoleLabelTwigExtension() {
+
+        // Set a Label Twig extension mock.
+        $labelTwigExtension = new LabelTwigExtension($this->twigEnvironment);
+
+        // Set a role label Twig extension mock.
+        $formButtonTwigExtension = new RoleLabelTwigExtension($this->twigEnvironment, $this->translator, $labelTwigExtension);
+
+        $obj = new TestRoleLabelTwigExtensionTrait();
+
+        $obj->setRoleLabelTwigExtension($formButtonTwigExtension);
+        $this->assertSame($formButtonTwigExtension, $obj->getRoleLabelTwigExtension());
+    }
+
+}
---
+++ Twig/Extension/Utility/RoleLabelTwigExtensionTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Twig\Extension\Utility;
+
+/**
+ * Form button Twig extension trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Twig\Extension\Utility
+ */
+trait RoleLabelTwigExtensionTrait {
+
+    /**
+     * Role label Twig extension.
+     *
+     * @var RoleLabelTwigExtension
+     */
+    private $roleLabelTwigExtension;
+
+    /**
+     * Get the role label Twig extention.
+     *
+     * @return RoleLabelTwigExtension Returns the role label Twig extension.
+     */
+    public function getRoleLabelTwigExtension() {
+        return $this->roleLabelTwigExtension;
+    }
+
+    /**
+     * Set the role label Twig extension.
+     *
+     * @param RoleLabelTwigExtension $roleLabelTwigExtension The role label Twig extension.
+     */
+    protected function setRoleLabelTwigExtension(RoleLabelTwigExtension $roleLabelTwigExtension = null) {
+        $this->roleLabelTwigExtension = $roleLabelTwigExtension;
+        return $this;
+    }
+
+}
