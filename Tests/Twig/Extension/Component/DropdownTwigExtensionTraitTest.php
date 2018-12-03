69cab4dbb2527b3230061cc05a58208d751482a2
webeweb <support@webeweb.fr>
2018-12-03 18:35:08 +0100

    Add Dropdown Twig extension trait

---
+++ Tests/Twig/Extension/Component/DropdownTwigExtensionTraitTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Component;
+
+use WBW\Bundle\BootstrapBundle\Tests\Fixtures\Twig\Extension\Component\TestDropdownTwigExtensionTrait;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\DropdownTwigExtension;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+
+/**
+ * Dropdown Twig extension trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Component
+ */
+class DropdownTwigExtensionTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TestDropdownTwigExtensionTrait();
+
+        $this->assertNull($obj->getDropdownTwigExtension());
+    }
+
+    /**
+     * Tests the setDropdownTwigExtension() method.
+     *
+     * @return void
+     */
+    public function testSetDropdownTwigExtension() {
+
+        // Set a Dropdown Twig extension mock.
+        $dropdownTwigExtension = new DropdownTwigExtension($this->twigEnvironment);
+
+        $obj = new TestDropdownTwigExtensionTrait();
+
+        $obj->setDropdownTwigExtension($dropdownTwigExtension);
+        $this->assertSame($dropdownTwigExtension, $obj->getDropdownTwigExtension());
+    }
+
+}
---
+++ Twig/Extension/Component/DropdownTwigExtensionTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Twig\Extension\Component;
+
+/**
+ * Dropdown Twig extension trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Twig\Extension\Component
+ */
+trait DropdownTwigExtensionTrait {
+
+    /**
+     * Dropdown Twig extension.
+     *
+     * @var DropdownTwigExtension
+     */
+    private $dropdownTwigExtension;
+
+    /**
+     * Get the dropdown Twig extention.
+     *
+     * @return DropdownTwigExtension Returns the dropdown Twig extension.
+     */
+    public function getDropdownTwigExtension() {
+        return $this->dropdownTwigExtension;
+    }
+
+    /**
+     * Set the dropdown Twig extension.
+     *
+     * @param DropdownTwigExtension $dropdownTwigExtension The dropdown Twig extension.
+     */
+    protected function setDropdownTwigExtension(DropdownTwigExtension $dropdownTwigExtension = null) {
+        $this->dropdownTwigExtension = $dropdownTwigExtension;
+        return $this;
+    }
+
+}
