6b1bbaa49ec5fe958c47a197e23c4d29caeb4961
webeweb <support@webeweb.fr>
2018-12-03 18:35:37 +0100

    Add Nav Twig extension trait

---
+++ Tests/Twig/Extension/Component/NavTwigExtensionTraitTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Component;
+
+use WBW\Bundle\BootstrapBundle\Tests\Fixtures\Twig\Extension\Component\TestNavTwigExtensionTrait;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\NavTwigExtension;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+
+/**
+ * Nav Twig extension trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Component
+ */
+class NavTwigExtensionTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TestNavTwigExtensionTrait();
+
+        $this->assertNull($obj->getNavTwigExtension());
+    }
+
+    /**
+     * Tests the setNavTwigExtension() method.
+     *
+     * @return void
+     */
+    public function testSetNavTwigExtension() {
+
+        // Set a Nav Twig extension mock.
+        $navTwigExtension = new NavTwigExtension($this->twigEnvironment);
+
+        $obj = new TestNavTwigExtensionTrait();
+
+        $obj->setNavTwigExtension($navTwigExtension);
+        $this->assertSame($navTwigExtension, $obj->getNavTwigExtension());
+    }
+
+}
---
+++ Twig/Extension/Component/NavTwigExtensionTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Twig\Extension\Component;
+
+/**
+ * Nav Twig extension trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Twig\Extension\Component
+ */
+trait NavTwigExtensionTrait {
+
+    /**
+     * Nav Twig extension.
+     *
+     * @var NavTwigExtension
+     */
+    private $navTwigExtension;
+
+    /**
+     * Get the nav Twig extention.
+     *
+     * @return NavTwigExtension Returns the nav Twig extension.
+     */
+    public function getNavTwigExtension() {
+        return $this->navTwigExtension;
+    }
+
+    /**
+     * Set the nav Twig extension.
+     *
+     * @param NavTwigExtension $navTwigExtension The nav Twig extension.
+     */
+    protected function setNavTwigExtension(NavTwigExtension $navTwigExtension = null) {
+        $this->navTwigExtension = $navTwigExtension;
+        return $this;
+    }
+
+}
