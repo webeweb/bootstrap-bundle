dcb25b81e4f30b5715c42ca54914acd32d4de13f
webeweb <support@webeweb.fr>
2018-12-03 18:35:18 +0100

    Add Glyphicon Twig extension trait

---
+++ Tests/Twig/Extension/Component/GlyphiconTwigExtensionTraitTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Component;
+
+use WBW\Bundle\BootstrapBundle\Tests\Fixtures\Twig\Extension\Component\TestGlyphiconTwigExtensionTrait;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\GlyphiconTwigExtension;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+
+/**
+ * Glyphicon Twig extension trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Component
+ */
+class GlyphiconTwigExtensionTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TestGlyphiconTwigExtensionTrait();
+
+        $this->assertNull($obj->getGlyphiconTwigExtension());
+    }
+
+    /**
+     * Tests the setGlyphiconTwigExtension() method.
+     *
+     * @return void
+     */
+    public function testSetGlyphiconTwigExtension() {
+
+        // Set a Glyphicon Twig extension mock.
+        $glyphiconTwigExtension = new GlyphiconTwigExtension($this->twigEnvironment);
+
+        $obj = new TestGlyphiconTwigExtensionTrait();
+
+        $obj->setGlyphiconTwigExtension($glyphiconTwigExtension);
+        $this->assertSame($glyphiconTwigExtension, $obj->getGlyphiconTwigExtension());
+    }
+
+}
---
+++ Twig/Extension/Component/GlyphiconTwigExtensionTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Twig\Extension\Component;
+
+/**
+ * Glyphicon Twig extension trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Twig\Extension\Component
+ */
+trait GlyphiconTwigExtensionTrait {
+
+    /**
+     * Glyphicon Twig extension.
+     *
+     * @var GlyphiconTwigExtension
+     */
+    private $glyphiconTwigExtension;
+
+    /**
+     * Get the glyphicon Twig extention.
+     *
+     * @return GlyphiconTwigExtension Returns the glyphicon Twig extension.
+     */
+    public function getGlyphiconTwigExtension() {
+        return $this->glyphiconTwigExtension;
+    }
+
+    /**
+     * Set the glyphicon Twig extension.
+     *
+     * @param GlyphiconTwigExtension $glyphiconTwigExtension The glyphicon Twig extension.
+     */
+    protected function setGlyphiconTwigExtension(GlyphiconTwigExtension $glyphiconTwigExtension = null) {
+        $this->glyphiconTwigExtension = $glyphiconTwigExtension;
+        return $this;
+    }
+
+}
