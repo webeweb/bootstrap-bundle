997e67e7295b6a7125296ebe18d2d7d4c7a8a015
webeweb <support@webeweb.fr>
2018-12-09 14:15:09 +0100

    Add default footer theme provider

---
+++ Tests/Theme/DefaultFooterThemeProviderTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Theme;
+
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\BootstrapBundle\Theme\DefaultFooterThemeProvider;
+
+/**
+ * Default footer theme provider test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Theme
+ */
+class DefaultFooterThemeProviderTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new DefaultFooterThemeProvider();
+
+        $this->assertEquals(null, $obj->getView());
+    }
+
+}
