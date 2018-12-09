3044bf1bcb14e43cf95074222ce98a3e4ad6450a
webeweb <support@webeweb.fr>
2018-12-09 14:15:23 +0100

    Add default hook drop down theme provider

---
+++ Theme/DefaultHookDropDownThemeProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Theme;
+
+use WBW\Bundle\CoreBundle\Provider\Theme\HookDropDownThemeProviderInterface;
+
+/**
+ * Default hook drop down theme provider.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Theme
+ */
+class DefaultHookDropDownThemeProvider implements HookDropDownThemeProviderInterface {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO.
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getItems() {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getView() {
+        return null;
+    }
+
+}
