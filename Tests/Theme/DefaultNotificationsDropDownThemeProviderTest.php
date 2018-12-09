e596fc9c5dd5901b4da7ec5112efceb038330ad0
webeweb <support@webeweb.fr>
2018-12-09 14:15:36 +0100

    Add default notifications drop down theme provider

---
+++ Tests/Theme/DefaultNotificationsDropDownThemeProviderTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Theme;
+
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\BootstrapBundle\Theme\DefaultNotificationsDropDownThemeProvider;
+
+/**
+ * Default notifications drop down theme provider test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Theme
+ */
+class DefaultNotificationsDropDownThemeProviderTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new DefaultNotificationsDropDownThemeProvider();
+
+        $this->assertEquals([], $obj->getNotifications());
+        $this->assertEquals(null, $obj->getView());
+    }
+
+}
