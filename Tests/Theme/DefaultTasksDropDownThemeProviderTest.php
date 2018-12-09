2b21e3c893e4332b4cf43d1c4a04b043f070c628
webeweb <support@webeweb.fr>
2018-12-09 14:15:52 +0100

    Add default tasks drop down theme provider

---
+++ Tests/Theme/DefaultTasksDropDownThemeProviderTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Theme;
+
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\BootstrapBundle\Theme\DefaultTasksDropDownThemeProvider;
+
+/**
+ * Default tasks drop down theme provider test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Theme
+ */
+class DefaultTasksDropDownThemeProviderTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new DefaultTasksDropDownThemeProvider();
+
+        $this->assertEquals([], $obj->getTasks());
+        $this->assertEquals(null, $obj->getView());
+    }
+
+}
