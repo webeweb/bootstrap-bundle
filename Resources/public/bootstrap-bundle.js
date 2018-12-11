5874474d210c3faa5f165b26703f6d7e622e6932
webeweb <support@webeweb.fr>
2018-12-11 17:15:52 +0100

    Empty bootstrap-bundle.js

--- Resources/public/bootstrap-bundle.js
+++ Resources/public/bootstrap-bundle.js
@@ -7,28 +7,3 @@
  * file that was distributed with this source code.
  */
 
-/**
- * Displays a SweetAlert "Error".
- *
- * @param {String} message The message.
- * @returns {undefined}
- */
-function bbSweetAlertError(message) {
-    swal({
-        icon: "error",
-        text: message
-    });
-} // bbSweetAlertError
-
-/**
- * Displays a SweetAlert "Success".
- *
- * @param {String} message The message.
- * @returns {undefined}
- */
-function bbSweetAlertSuccess(message) {
-    swal({
-        icon: "success",
-        text: message
-    });
-} // bbSweetAlertError
