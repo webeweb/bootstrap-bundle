0f384934e6d48f61e71e7743cf268eb81f033152
webeweb <support@webeweb.fr>
2018-12-22 12:40:06 +0100

    Complete Label Twig extension unit tests

--- Tests/Twig/Extension/Component/LabelTwigExtensionTest.php
+++ Tests/Twig/Extension/Component/LabelTwigExtensionTest.php
@@ -25,6 +25,19 @@ use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\LabelTwigExtension;
 class LabelTwigExtensionTest extends AbstractTestCase {
 
     /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new LabelTwigExtension($this->twigEnvironment);
+
+        $this->assertEquals("webeweb.bootstrap.twig.extension.component.label", LabelTwigExtension::SERVICE_NAME);
+        $this->assertSame($this->twigEnvironment, $obj->getTwigEnvironment());
+    }
+
+    /**
      * Tests the getFunctions() method.
      *
      * @return void
