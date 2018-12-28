3601b7881fe2556d5828986660a1dab7edc3f609
webeweb <support@webeweb.fr>
2018-12-28 14:11:12 +0100

    Code style

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -10,6 +10,6 @@
  */
 if (true === file_exists($file = __DIR__ . "/autoload.php")) {
     require_once $file;
-} elseif (true === file_exists($file = __DIR__ . "/autoload.php.dist")) {
+} else if (true === file_exists($file = __DIR__ . "/autoload.php.dist")) {
     require_once $file;
 }
