71a0dc8eafa33fe44e9889d40ac6f5115b5f98ba
webeweb <support@webeweb.fr>
2018-12-30 15:15:50 +0100

    Add Grid helper

---
+++ Helper/GridHelper.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Helper;
+
+/**
+ * Grid helper.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Helper
+ */
+class GridHelper {
+
+    /**
+     * Get a CSS classname.
+     *
+     * @param string $size The size.
+     * @param int $value The value.
+     * @param string $suffix The suffix.
+     * @param int $min The min value.
+     * @param int $max The max value.
+     * @return string Returns the CSS classname.
+     */
+    public static function getCSSClassname($size, $value, $suffix, $min = 1, $max = 12) {
+
+        // Check the value.
+        if ($value < $min || $max < $value) {
+            return "";
+        }
+
+        // Initialize the values.
+        $sizes    = ["lg", "md", "sm", "xs"];
+        $suffixes = ["offset", "pull", "push", ""];
+
+        // Check the size.
+        if (false === in_array($size, $sizes)) {
+            return "";
+        }
+
+        // Check the suffixes.
+        if (false === in_array($suffix, $suffixes)) {
+            return "";
+        }
+        if ("" !== $suffix) {
+            $suffix .= "-";
+        }
+
+        // Returns the classname.
+        return implode("-", ["col", $size, $suffix . $value]);
+    }
+}
---
+++ Tests/Helper/GridHelperTest.php
@@ -0,0 +1,136 @@
+<?php
+
+/*
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Helper;
+
+use WBW\Bundle\BootstrapBundle\Helper\GridHelper;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Grid helper test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Helper
+ */
+class GridHelperTest extends AbstractTestCase {
+
+    /**
+     * Tests the getCSSClassname() method.
+     *
+     * @return void
+     */
+    public function testGetCSSClassnameWithBadSize() {
+
+        $res = "";
+        $this->assertEquals($res, GridHelper::getCSSClassname("", 6, ""));
+    }
+
+    /**
+     * Tests the getCSSClassname() method.
+     *
+     * @return void
+     */
+    public function testGetCSSClassnameWithBadSuffix() {
+
+        $res = "";
+        $this->assertEquals($res, GridHelper::getCSSClassname("lg", 6, "suffix"));
+    }
+
+    /**
+     * Tests the getCSSClassname() method.
+     *
+     * @return void
+     */
+    public function testGetCSSClassnameWithBadValue() {
+
+        $res = "";
+        $this->assertEquals($res, GridHelper::getCSSClassname("lg", 0, ""));
+        $this->assertEquals($res, GridHelper::getCSSClassname("lg", 13, ""));
+    }
+
+    /**
+     * Tests the getCSSClassname() method.
+     *
+     * @return void
+     */
+    public function testGetCSSClassnameWithSizeLg() {
+
+        $res = "col-lg-6";
+        $this->assertEquals($res, GridHelper::getCSSClassname("lg", 6, ""));
+    }
+
+    /**
+     * Tests the getCSSClassname() method.
+     *
+     * @return void
+     */
+    public function testGetCSSClassnameWithSizeMd() {
+
+        $res = "col-md-6";
+        $this->assertEquals($res, GridHelper::getCSSClassname("md", 6, ""));
+    }
+
+    /**
+     * Tests the getCSSClassname() method.
+     *
+     * @return void
+     */
+    public function testGetCSSClassnameWithSizeSm() {
+
+        $res = "col-sm-6";
+        $this->assertEquals($res, GridHelper::getCSSClassname("sm", 6, ""));
+    }
+
+    /**
+     * Tests the getCSSClassname() method.
+     *
+     * @return void
+     */
+    public function testGetCSSClassnameWithSizeXs() {
+
+        $res = "col-xs-6";
+        $this->assertEquals($res, GridHelper::getCSSClassname("xs", 6, ""));
+    }
+
+    /**
+     * Tests the getCSSClassname() method.
+     *
+     * @return void
+     */
+    public function testGetCSSClassnameWithSuffixOffset() {
+
+        $res = "col-lg-offset-6";
+        $this->assertEquals($res, GridHelper::getCSSClassname("lg", 6, "offset"));
+    }
+
+    /**
+     * Tests the getCSSClassname() method.
+     *
+     * @return void
+     */
+    public function testGetCSSClassnameWithSuffixPull() {
+
+        $res = "col-lg-pull-6";
+        $this->assertEquals($res, GridHelper::getCSSClassname("lg", 6, "pull"));
+    }
+
+    /**
+     * Tests the getCSSClassname() method.
+     *
+     * @return void
+     */
+    public function testGetCSSClassnameWithSuffixPush() {
+
+        $res = "col-lg-push-6";
+        $this->assertEquals($res, GridHelper::getCSSClassname("lg", 6, "push"));
+    }
+
+}
