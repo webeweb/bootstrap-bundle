de59c932446d6bb377e4a86bdb63d13beb86533c
webeweb <support@webeweb.fr>
2019-01-13 15:30:25 +0100

    Add Default button

---
+++ Tests/Button/DefaultButtonTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Button;
+
+use WBW\Bundle\BootstrapBundle\Button\ButtonInterface;
+use WBW\Bundle\BootstrapBundle\Button\DefaultButton;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Default button test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Button
+ */
+class DefaultButtonTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new DefaultButton();
+
+        $this->assertEquals(ButtonInterface::BUTTON_TYPE_DEFAULT, $obj->getType());
+    }
+
+}
