4f5b4dd5e425facf0eab32468bb678903c31cbce
webeweb <support@webeweb.fr>
2019-01-13 15:31:01 +0100

    Add Success button

---
+++ Tests/Button/SuccessButtonTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Button;
+
+use WBW\Bundle\BootstrapBundle\Button\ButtonInterface;
+use WBW\Bundle\BootstrapBundle\Button\SuccessButton;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Success button test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Button
+ */
+class SuccessButtonTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new SuccessButton();
+
+        $this->assertEquals(ButtonInterface::BUTTON_TYPE_SUCCESS, $obj->getType());
+    }
+
+}
