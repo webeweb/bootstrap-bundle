5c9b87db73769350972bca92c23a2d8166e9465a
webeweb <support@webeweb.fr>
2019-01-13 15:00:02 +0100

    Add abstract button

---
+++ Button/AbstractButton.php
@@ -0,0 +1,189 @@
+<?php
+
+/*
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Button;
+
+/**
+ * Abstract button.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Button
+ * @abstract
+ */
+abstract class AbstractButton implements ButtonInterface {
+
+    /**
+     * Active
+     *
+     * @var bool
+     */
+    private $active;
+
+    /**
+     * Block.
+     *
+     * @var bool
+     */
+    private $block;
+
+    /**
+     * Content.
+     *
+     * @var string
+     */
+    private $content;
+
+    /**
+     * Disabled.
+     *
+     * @var bool
+     */
+    private $disabled;
+
+    /**
+     * Size.
+     *
+     * @var string
+     */
+    private $size;
+
+    /**
+     * Title.
+     *
+     * @var string
+     */
+    private $title;
+
+    /**
+     * Type.
+     *
+     * @var string
+     */
+    private $type;
+
+    /**
+     * Constructor.
+     *
+     * @param string type.
+     */
+    protected function __construct($type) {
+        $this->setActive(false);
+        $this->setBlock(false);
+        $this->setDisabled(false);
+        $this->setType($type);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getActive() {
+        return $this->active;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlock() {
+        return $this->block;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getContent() {
+        return $this->content;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDisabled() {
+        return $this->disabled;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSize() {
+        return $this->size;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTitle() {
+        return $this->title;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getType() {
+        return $this->type;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setActive($active) {
+        $this->active = $active;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setBlock($block) {
+        $this->block = $block;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContent($content) {
+        $this->content = $content;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDisabled($disabled) {
+        $this->disabled = $disabled;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setSize($size) {
+        $this->size = $size;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTitle($title) {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setType($type) {
+        $this->type = $type;
+        return $this;
+    }
+
+}
---
+++ Tests/Button/AbstractButtonTest.php
@@ -0,0 +1,121 @@
+<?php
+
+/*
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Button;
+
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Bundle\BootstrapBundle\Tests\Fixtures\Button\TestButton;
+
+/**
+ * Abstract button test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Button
+ */
+class AbstractButtonTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TestButton("type");
+
+        $this->assertFalse($obj->getActive());
+        $this->assertFalse($obj->getBlock());
+        $this->assertNull($obj->getContent());
+        $this->assertFalse($obj->getDisabled());
+        $this->assertNull($obj->getSize());
+        $this->assertNull($obj->getTitle());
+        $this->assertEquals("type", $obj->getType());
+    }
+
+    /**
+     * Tests the setActive() method.
+     *
+     * @return void
+     */
+    public function testSetActive() {
+
+        $obj = new TestButton("type");
+
+        $obj->setActive(true);
+        $this->assertTrue($obj->getActive());
+    }
+
+    /**
+     * Tests the setBlock() method.
+     *
+     * @return void
+     */
+    public function testSetBlock() {
+
+        $obj = new TestButton("type");
+
+        $obj->setBlock(true);
+        $this->assertTrue($obj->getBlock());
+    }
+
+    /**
+     * Tests the setContent() method.
+     *
+     * @return void
+     */
+    public function testSetContent() {
+
+        $obj = new TestButton("type");
+
+        $obj->setContent("content");
+        $this->assertEquals("content", $obj->getContent());
+    }
+
+    /**
+     * Tests the setDisabled() method.
+     *
+     * @return void
+     */
+    public function testSetDisabled() {
+
+        $obj = new TestButton("type");
+
+        $obj->setDisabled(true);
+        $this->assertTrue($obj->getDisabled());
+    }
+
+    /**
+     * Tests the setSize() method.
+     *
+     * @return void
+     */
+    public function testSetSize() {
+
+        $obj = new TestButton("type");
+
+        $obj->setSize("size");
+        $this->assertEquals("size", $obj->getSize());
+    }
+
+    /**
+     * Tests the setTitle() method.
+     *
+     * @return void
+     */
+    public function testSetTitle() {
+
+        $obj = new TestButton("type");
+
+        $obj->setTitle("title");
+        $this->assertEquals("title", $obj->getTitle());
+    }
+
+}
