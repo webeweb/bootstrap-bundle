40ac98a1c2de77b698aa1ead2edcfb2f7eeff2eb
webeweb <support@webeweb.fr>
2019-01-14 18:36:28 +0100

    Add Button enumerator

---
+++ Button/ButtonEnumerator.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Button;
+
+/**
+ * Button enumerator.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Button
+ */
+class ButtonEnumerator {
+
+    /**
+     * Enumerates the sizes.
+     *
+     * @return array Returns the sizes enumeration.
+     */
+    public static function enumSizes() {
+        return [
+            ButtonInterface::BUTTON_SIZE_LG,
+            ButtonInterface::BUTTON_SIZE_SM,
+            ButtonInterface::BUTTON_SIZE_XS,
+        ];
+    }
+
+    /**
+     * Enumerates the types.
+     *
+     * @return array Returns the types enumeration.
+     */
+    public static function enumTypes() {
+        return [
+            ButtonInterface::BUTTON_TYPE_DANGER,
+            ButtonInterface::BUTTON_TYPE_DEFAULT,
+            ButtonInterface::BUTTON_TYPE_INFO,
+            ButtonInterface::BUTTON_TYPE_PRIMARY,
+            ButtonInterface::BUTTON_TYPE_SUCCESS,
+            ButtonInterface::BUTTON_TYPE_WARNING,
+        ];
+    }
+}
---
+++ Tests/Button/ButtonEnumeratorTest.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Button;
+
+use WBW\Bundle\BootstrapBundle\Button\ButtonEnumerator;
+use WBW\Bundle\BootstrapBundle\Button\ButtonInterface;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+class ButtonEnumeratorTest extends AbstractTestCase {
+
+    /**
+     * Tests the enumSizes() method.
+     *
+     * @return void
+     */
+    public function testEnumSizes() {
+
+        $res = [
+            ButtonInterface::BUTTON_SIZE_LG,
+            ButtonInterface::BUTTON_SIZE_SM,
+            ButtonInterface::BUTTON_SIZE_XS,
+        ];
+        $this->assertEquals($res, ButtonEnumerator::enumSizes());
+    }
+
+    /**
+     * Tests the enumTypes() method.
+     *
+     * @return void
+     */
+    public function testEnumTypes() {
+
+        $res = [
+            ButtonInterface::BUTTON_TYPE_DANGER,
+            ButtonInterface::BUTTON_TYPE_DEFAULT,
+            ButtonInterface::BUTTON_TYPE_INFO,
+            ButtonInterface::BUTTON_TYPE_PRIMARY,
+            ButtonInterface::BUTTON_TYPE_SUCCESS,
+            ButtonInterface::BUTTON_TYPE_WARNING,
+        ];
+        $this->assertEquals($res, ButtonEnumerator::enumTypes());
+    }
+
+}
