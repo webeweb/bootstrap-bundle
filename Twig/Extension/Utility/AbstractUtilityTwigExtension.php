ae73eca570963319074a869ceffccdd4a2ca4315
webeweb <support@webeweb.fr>
2019-02-15 16:48:38 +0100

    Update constructor visibility

--- Twig/Extension/Utility/AbstractUtilityTwigExtension.php
+++ Twig/Extension/Utility/AbstractUtilityTwigExtension.php
@@ -33,7 +33,7 @@ abstract class AbstractUtilityTwigExtension extends AbstractTwigExtension {
      * @param Twig_Environment $twigEnvironment The Twig environment.
      * @param TranslatorInterface $translator The translator.
      */
-    protected function __construct(Twig_Environment $twigEnvironment, TranslatorInterface $translator) {
+    public function __construct(Twig_Environment $twigEnvironment, TranslatorInterface $translator) {
         parent::__construct($twigEnvironment);
         $this->setTranslator($translator);
     }
