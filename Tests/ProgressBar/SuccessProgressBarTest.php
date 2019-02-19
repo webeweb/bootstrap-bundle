8c09b7efc2a0b69a9f626d2d2d119d498fd9cf36
webeweb <support@webeweb.fr>
2019-02-19 17:52:33 +0100

    Add Success progress bar

---
+++ Tests/ProgressBar/SuccessProgressBarTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\ProgressBar;
+
+use WBW\Bundle\BootstrapBundle\ProgressBar\SuccessProgressBar;
+use WBW\Bundle\BootstrapBundle\ProgressBar\ProgressBarInterface;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Success progress bar test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\ProgressBar
+ */
+class SuccessProgressBarTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new SuccessProgressBar();
+
+        $this->assertEquals(ProgressBarInterface::PROGRESS_BAR_TYPE_SUCCESS, $obj->getType());
+    }
+}
