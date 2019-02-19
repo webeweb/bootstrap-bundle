f8f98ee6064bc2a887be0d9faf7ad7e7708fc9be
webeweb <support@webeweb.fr>
2019-02-19 17:51:29 +0100

    Add Basic progress bar

---
+++ Tests/ProgressBar/BasicProgressBarTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license basicrmation, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\ProgressBar;
+
+use WBW\Bundle\BootstrapBundle\ProgressBar\BasicProgressBar;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Basic progress bar test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\ProgressBar
+ */
+class BasicProgressBarTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new BasicProgressBar();
+
+        $this->assertNull($obj->getType());
+    }
+}
