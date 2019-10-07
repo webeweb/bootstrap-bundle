e7914951371b3189f77c7aade12312abf8b214d9
webeweb <support@webeweb.fr>
2019-10-07 20:43:43 +0200

    Add Info alert

---
+++ Tests/Alert/InfoAlertTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Alert;
+
+use WBW\Bundle\BootstrapBundle\Alert\AlertInterface;
+use WBW\Bundle\BootstrapBundle\Alert\InfoAlert;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Info alert test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Alert
+ */
+class InfoAlertTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new InfoAlert();
+
+        $this->assertEquals(AlertInterface::ALERT_TYPE_INFO, $obj->getType());
+    }
+}
