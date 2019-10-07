05f033b777f6b78ca08420b9ee199de23fbfb77c
webeweb <support@webeweb.fr>
2019-10-07 20:44:12 +0200

    Add Secondary alert

---
+++ Tests/Alert/SecondaryAlertTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Alert;
+
+use WBW\Bundle\BootstrapBundle\Alert\AlertInterface;
+use WBW\Bundle\BootstrapBundle\Alert\SecondaryAlert;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Secondary alert test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Alert
+ */
+class SecondaryAlertTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new SecondaryAlert();
+
+        $this->assertEquals(AlertInterface::ALERT_TYPE_SECONDARY, $obj->getType());
+    }
+}
