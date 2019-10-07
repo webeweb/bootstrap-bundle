696bb44a787f7a90721c982370c06c0b549d9cf4
webeweb <support@webeweb.fr>
2019-10-07 20:44:02 +0200

    Add Primary alert

---
+++ Tests/Alert/PrimaryAlertTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Alert;
+
+use WBW\Bundle\BootstrapBundle\Alert\AlertInterface;
+use WBW\Bundle\BootstrapBundle\Alert\PrimaryAlert;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Primary alert test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Alert
+ */
+class PrimaryAlertTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new PrimaryAlert();
+
+        $this->assertEquals(AlertInterface::ALERT_TYPE_PRIMARY, $obj->getType());
+    }
+}
