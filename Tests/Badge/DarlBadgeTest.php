fa34a7e4484466af41c254638a9a679320fd14ea
webeweb <support@webeweb.fr>
2019-10-07 20:51:05 +0200

    Add Dark badge

---
+++ Tests/Badge/DarlBadgeTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Badge;
+
+use WBW\Bundle\BootstrapBundle\Badge\BadgeInterface;
+use WBW\Bundle\BootstrapBundle\Badge\DarkBadge;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Dark badge test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Badge
+ */
+class DarkBadgeTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new DarkBadge();
+
+        $this->assertEquals(BadgeInterface::BADGE_TYPE_DARK, $obj->getType());
+    }
+}
