6849cc4b21d13bcee9d4fabd54c7604aba721348
webeweb <support@webeweb.fr>
2019-10-07 20:45:35 +0200

    Add Alert enumerator unit tests

---
+++ Tests/Alert/AlertEnumeratorTest.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Alert;
+
+use WBW\Bundle\BootstrapBundle\Alert\AlertEnumerator;
+use WBW\Bundle\BootstrapBundle\Alert\AlertInterface;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Alert enumerator test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Alert
+ */
+class AlertEnumeratorTest extends AbstractTestCase {
+
+    /**
+     * Tests the enumTypes() method.
+     *
+     * @return void
+     */
+    public function testEnumTypes() {
+
+        $res = [
+            AlertInterface::ALERT_TYPE_DANGER,
+            AlertInterface::ALERT_TYPE_DARK,
+            AlertInterface::ALERT_TYPE_INFO,
+            AlertInterface::ALERT_TYPE_LIGHT,
+            AlertInterface::ALERT_TYPE_PRIMARY,
+            AlertInterface::ALERT_TYPE_SECONDARY,
+            AlertInterface::ALERT_TYPE_SUCCESS,
+            AlertInterface::ALERT_TYPE_WARNING,
+        ];
+        $this->assertEquals($res, AlertEnumerator::enumTypes());
+    }
+
+}
