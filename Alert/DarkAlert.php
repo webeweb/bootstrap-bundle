7a619e56043a6a8bc7503597bfd4bb81c59afed8
webeweb <support@webeweb.fr>
2019-10-07 20:43:33 +0200

    Add Dark alert

---
+++ Alert/DarkAlert.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Alert;
+
+/**
+ * Dark alert.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Alert
+ */
+class DarkAlert extends AbstractAlert {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct(self::ALERT_TYPE_DARK);
+    }
+}
---
+++ Tests/Alert/DarlAlertTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Alert;
+
+use WBW\Bundle\BootstrapBundle\Alert\AlertInterface;
+use WBW\Bundle\BootstrapBundle\Alert\DarkAlert;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Dark alert test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Alert
+ */
+class DarkAlertTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new DarkAlert();
+
+        $this->assertEquals(AlertInterface::ALERT_TYPE_DARK, $obj->getType());
+    }
+}
