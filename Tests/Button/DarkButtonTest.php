d732e1951f00241c874e87adfc2721b17f3198c6
webeweb <support@webeweb.fr>
2019-10-08 19:03:07 +0200

    Add Dark button

---
+++ Tests/Button/DarkButtonTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Button;
+
+use WBW\Bundle\BootstrapBundle\Button\ButtonInterface;
+use WBW\Bundle\BootstrapBundle\Button\DarkButton;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Dark button test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Button
+ */
+class DarkButtonTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new DarkButton();
+
+        $this->assertEquals(ButtonInterface::BUTTON_TYPE_DARK, $obj->getType());
+    }
+}
