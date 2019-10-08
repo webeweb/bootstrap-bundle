2726559e6366beba6d9dc6e984c17793fce90034
webeweb <support@webeweb.fr>
2019-10-08 19:03:32 +0200

    Add Secondary button

---
+++ Tests/Button/SecondaryButtonTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Button;
+
+use WBW\Bundle\BootstrapBundle\Button\ButtonInterface;
+use WBW\Bundle\BootstrapBundle\Button\SecondaryButton;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Secondary button test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Button
+ */
+class SecondaryButtonTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new SecondaryButton();
+
+        $this->assertEquals(ButtonInterface::BUTTON_TYPE_SECONDARY, $obj->getType());
+    }
+}
