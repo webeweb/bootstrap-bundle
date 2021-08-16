8c5bc971b138b901f10e6e1e219c6e0ab6bc751a
webeweb <support@webeweb.fr>
2021-08-16 11:02:42 +0200

    Fix file header

--- Tests/Fixtures/app/TestKernel.php
+++ Tests/Fixtures/app/TestKernel.php
@@ -15,7 +15,6 @@ use WBW\Bundle\CoreBundle\Tests\AbstractKernel;
  * Test kernel.
  *
  * @author webeweb <https://github.com/webeweb/>
- * @package WBW\Bundle\BootstrapBundle\Tests\Fixtures\App
  */
 class TestKernel extends AbstractKernel {
 
