9829a8d58f19e350ea36b4a71b19513054f6a6f8
webeweb <support@webeweb.fr>
2021-09-28 10:23:15 +0200

    Add Bootstrap Datepicker locale support

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -11,6 +11,7 @@
 
 namespace WBW\Bundle\BootstrapBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 use WBW\Bundle\CoreBundle\DependencyInjection\ConfigurationHelper;
@@ -22,6 +23,106 @@ use WBW\Bundle\CoreBundle\DependencyInjection\ConfigurationHelper;
  * @package WBW\Bundle\BootstrapBundle\DependencyInjection
  */
 class Configuration implements ConfigurationInterface {
+
+    /**
+     * Add the Bootstrap Datepicker locales.
+     *
+     * @param ArrayNodeDefinition $node The locales node.
+     * @param array $plugins The plugins.
+     * @return void
+     */
+    private function addBootstrapDatepickerLocales(ArrayNodeDefinition $node, array $plugins): void {
+
+        $node
+            ->children()
+                ->variableNode("bootstrap_datepicker")->defaultValue("en-GB")->info("Bootstrap Datepicker locale")
+                    ->validate()
+                        ->ifNotInArray($plugins["bootstrap_datepicker"]["locales"])
+                        ->thenInvalid("The Bootstrap Datepicker locale %s is not supported. Please choose one of " . json_encode($plugins["bootstrap_datepicker"]["locales"]))
+                    ->end()
+                ->end()
+            ->end();
+    }
+
+    /**
+     * Add the Bootstrap Markdown locales.
+     *
+     * @param ArrayNodeDefinition $node The locales node.
+     * @param array $plugins The plugins.
+     * @return void
+     */
+    private function addBootstrapMarkdownLocales(ArrayNodeDefinition $node, array $plugins): void {
+
+        $node
+            ->children()
+                ->variableNode("bootstrap_markdown")->defaultValue("en")->info("Bootstrap Markdown locale")
+                    ->validate()
+                        ->ifNotInArray($plugins["bootstrap_markdown"]["locales"])
+                        ->thenInvalid("The Bootstrap Markdown locale %s is not supported. Please choose one of " . json_encode($plugins["bootstrap_markdown"]["locales"]))
+                    ->end()
+                ->end()
+            ->end();
+    }
+
+    /**
+     * Add the Bootstrap Select locales.
+     *
+     * @param ArrayNodeDefinition $node The locales node.
+     * @param array $plugins The plugins.
+     * @return void
+     */
+    private function addBootstrapSelectLocales(ArrayNodeDefinition $node, array $plugins): void {
+
+        $node
+            ->children()
+                ->variableNode("bootstrap_select")->defaultValue("en_US")->info("Bootstrap Select locale")
+                    ->validate()
+                        ->ifNotInArray($plugins["bootstrap_select"]["locales"])
+                        ->thenInvalid("The Bootstrap Select locale %s is not supported. Please choose one of " . json_encode($plugins["bootstrap_select"]["locales"]))
+                    ->end()
+                ->end()
+            ->end();
+    }
+
+    /**
+     * Add the Bootstrap WYSIWYG locales.
+     *
+     * @param ArrayNodeDefinition $node The locales node.
+     * @param array $plugins The plugins.
+     * @return void
+     */
+    private function addBootstrapWysiwigLocales(ArrayNodeDefinition $node, array $plugins): void {
+
+        $node
+            ->children()
+                ->variableNode("bootstrap_wysiwyg")->defaultValue("en-US")->info("Bootstrap WYSIWYG locale")
+                    ->validate()
+                        ->ifNotInArray($plugins["bootstrap_wysiwyg"]["locales"])
+                        ->thenInvalid("The Bootstrap WYSIWYG locale %s is not supported. Please choose one of " . json_encode($plugins["bootstrap_wysiwyg"]["locales"]))
+                    ->end()
+                ->end()
+            ->end();
+    }
+
+    /**
+     * Add the Summernote locales.
+     *
+     * @param ArrayNodeDefinition $node The locales node.
+     * @param array $plugins The plugins.
+     * @return void
+     */
+    private function addSummernoteLocales(ArrayNodeDefinition $node, array $plugins): void {
+
+        $node
+            ->children()
+                ->variableNode("summernote")->defaultValue("en-US")->info("Summernote locale")
+                    ->validate()
+                        ->ifNotInArray($plugins["summernote"]["locales"])
+                        ->thenInvalid("The Summernote locale %s is not supported. Please choose one of " . json_encode($plugins["summernote"]["locales"]))
+                    ->end()
+                ->end()
+            ->end();
+    }
 
     /**
      * {@inheritDoc}
@@ -36,8 +137,8 @@ class Configuration implements ConfigurationInterface {
         $rootNode = ConfigurationHelper::getRootNode($treeBuilder, WBWBootstrapExtension::EXTENSION_ALIAS);
         $rootNode
             ->children()
-                ->booleanNode("twig")->defaultTrue()->info("Load Twig extensions")->end()
                 ->integerNode("version")->defaultValue(4)->info("Version")->min(3)->max(5)->end()
+                ->booleanNode("twig")->defaultTrue()->info("Load Twig extensions")->end()
                 ->arrayNode("plugins")->info("Bootstrap plug-ins")
                     ->prototype("scalar")
                         ->validate()
@@ -47,34 +148,14 @@ class Configuration implements ConfigurationInterface {
                     ->end()
                 ->end()
                 ->arrayNode("locales")->addDefaultsIfNotSet()
-                    ->children()
-                        ->variableNode("bootstrap_markdown")->defaultValue("en")->info("Bootstrap Markdown locale")
-                            ->validate()
-                                ->ifNotInArray($plugins["bootstrap_markdown"]["locales"])
-                                ->thenInvalid("The Bootstrap Markdown locale %s is not supported. Please choose one of " . json_encode($plugins["bootstrap_markdown"]["locales"]))
-                            ->end()
-                        ->end()
-                        ->variableNode("bootstrap_select")->defaultValue("en_US")->info("Bootstrap Select locale")
-                            ->validate()
-                                ->ifNotInArray($plugins["bootstrap_select"]["locales"])
-                                ->thenInvalid("The Bootstrap Select locale %s is not supported. Please choose one of " . json_encode($plugins["bootstrap_select"]["locales"]))
-                            ->end()
-                        ->end()
-                        ->variableNode("bootstrap_wysiwyg")->defaultValue("en-US")->info("Bootstrap WYSIWYG locale")
-                            ->validate()
-                                ->ifNotInArray($plugins["bootstrap_wysiwyg"]["locales"])
-                                ->thenInvalid("The Bootstrap WYSIWYG locale %s is not supported. Please choose one of " . json_encode($plugins["bootstrap_wysiwyg"]["locales"]))
-                            ->end()
-                        ->end()
-                        ->variableNode("summernote")->defaultValue("en-US")->info("Summernote locale")
-                            ->validate()
-                                ->ifNotInArray($plugins["summernote"]["locales"])
-                                ->thenInvalid("The Summernote locale %s is not supported. Please choose one of " . json_encode($plugins["summernote"]["locales"]))
-                            ->end()
-                        ->end()
-                    ->end()
-                ->end()
             ->end();
+
+        $localesNode = $rootNode->find("locales");
+        $this->addBootstrapDatepickerLocales($localesNode, $plugins);
+        $this->addBootstrapMarkdownLocales($localesNode, $plugins);
+        $this->addBootstrapSelectLocales($localesNode, $plugins);
+        $this->addBootstrapWysiwigLocales($localesNode, $plugins);
+        $this->addSummernoteLocales($localesNode, $plugins);
 
         return $treeBuilder;
     }
