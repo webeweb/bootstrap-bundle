864f557706d0ed7efebcffd5b53da6dfe3313578
webeweb <support@webeweb.fr>
2022-03-15 10:05:24 +0100

    Code checkstyle

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -127,7 +127,7 @@ class Configuration implements ConfigurationInterface {
     /**
      * {@inheritDoc}
      */
-    public function getConfigTreeBuilder() {
+    public function getConfigTreeBuilder(): TreeBuilder {
 
         $assets  = ConfigurationHelper::loadYamlConfig(__DIR__, "assets");
         $plugins = $assets["assets"]["wbw.bootstrap.asset.bootstrap"]["plugins"];
